d9da7fed703bf38a3015d859f437df47ac9b87a4
Luke Hollis <lukehollis@gmail.com>
2016-08-16 00:51:32 -0400

    sort subworks in search tools

--- client/shared/Header.jsx
+++ client/shared/Header.jsx
@@ -124,6 +124,14 @@ Header = React.createClass({
 			});
 
 		}else {
+			value.subworks.sort(function(a, b){
+			  if (a.n < b.n)
+			    return -1;
+			  if (a.n > b.n)
+			    return 1;
+			  return 0;
+
+			});
 			this.setState({
 				subworks: value.subworks,
 				activeWork: value.slug

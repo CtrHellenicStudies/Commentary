dd2fd8ba5ca361f8b524a69dfb33ae6249cf713d
jess <jess@misclabs.com>
2016-09-29 10:18:14 -0400

    Cleanup eslint errors on KeywordsList.jsx

--- client/views/keywords/KeywordsList.jsx
+++ client/views/keywords/KeywordsList.jsx
@@ -1,45 +1,40 @@
 KeywordsList = React.createClass({
 
+	propTypes: {
+		type: React.PropTypes.string.isRequired,
+	},
+
 	// This mixin makes the getMeteorData method keyword
 	mixins: [ReactMeteorData],
-
-	propTypes: {
-		type: React.PropTypes.string.isRequired
-	},
 
 	// Loads items from the keywords collection and puts them on this.data.keywords
 	getMeteorData() {
 		const query = {
-			type: this.props.type
+			type: this.props.type,
 		};
 
 		return {
-			keywords: Keywords.find(query, {sort: {title: 1}}).fetch()
+			keywords: Keywords.find(query, { sort: { title: 1 } }).fetch(),
 		};
 	},
 
 	renderKeywords() {
-
 		return this.data.keywords.map((keyword, i) => {
-			return <KeywordTeaser
-							key={i}
-							keyword={keyword} />;
-
+			return (
+				<KeywordTeaser
+					key={i}
+					keyword={keyword}
+				/>
+			);
 		});
-
 	},
 
 	render() {
-
-		 return (
-			 <div className="keywords-list">
-				 {this.renderKeywords()}
-
-			 </div>
-
-
-			);
-		}
-
+		return (
+			<div className="keywords-list">
+				{this.renderKeywords()}
+			</div>
+		);
+	},
 
 });

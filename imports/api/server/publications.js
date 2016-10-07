cfdccc3912a0edf93ebd626de9922f268828d5c6
jess <jess@misclabs.com>
2016-10-07 12:47:51 -0400

    add client input checks to keyword context subscriptions

--- imports/api/server/publications.js
+++ imports/api/server/publications.js
@@ -1,26 +1,26 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check'
+import { Match } from 'meteor/check'
 import { queryCommentWithKeyword } from '../api_utils.js';
 
 Meteor.publish('comments.keyword_context', function (keyword_slug) {
-	// TODO: check input
-	console.log('publishing comments.keyword_context for ', keyword_slug);
-	
+	check(keyword_slug, String);
+
 	return queryCommentWithKeyword(keyword_slug);
 });
 
 Meteor.publish('textnodes.keyword_context', function (lemma_query) {
-	// TODO: check input
+	check(lemma_query, {
+		'work.slug': String,
+		'subwork.n': Match.Integer,
+		'text.n': Match.Where((x) => {
+			check(x.$gte, Match.Integer);
+			check(x.$lte, Match.Integer);
+			return x.$lte >= x.$gte;
+		}),
+	});
 
-	const safeQuery = {
-		'work.slug': lemma_query['work.slug'],
-		'subwork.n': lemma_query['subwork.n'],
-		'text.n': {
-				$gte: lemma_query['text.n'].$gte,
-				$lte: lemma_query['text.n'].$lte,
-		}
-	}
-
-	return TextNodes.find(safeQuery, {
+	return TextNodes.find(lemma_query, {
 		limit: 50,
 		sort: { 'text.n':1 },
 		fields: { work:1, subwork:1, text:1 }

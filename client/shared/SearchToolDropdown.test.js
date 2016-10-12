cf9d79845006955d05227b246630d7404d7a020c
Suhaib Khan <suheb.work@gmail.com>
2016-10-13 00:03:58 +0530

    Adding assertions to check parent callbacks and adding SearchTermButtonPanel tests
    Signed-off-by: Suhaib Khan <suheb.work@gmail.com>

--- client/shared/SearchToolDropdown.test.js
+++ client/shared/SearchToolDropdown.test.js
@@ -12,9 +12,13 @@ import SearchToolDropdown from './SearchToolDropdown.jsx';
 if (Meteor.isClient) {
 	describe('SearchToolDropdown', () => {
 		it('should render', () => {
+			const toggle = sinon.spy();
 			const wrapper = shallow(
 				<SearchToolDropdown
 					name="TestDropdown"
+					toggle={toggle}
+					open={false}
+					disabled={false}
 				>
 					<p>children</p>
 				</SearchToolDropdown>
@@ -26,34 +30,62 @@ if (Meteor.isClient) {
 		});
 
 		it('should render with dropdown open', () => {
+			const toggle = sinon.spy();
 			const wrapper = shallow(
 				<SearchToolDropdown
 					name="TestDropdown"
+					toggle={toggle}
 					open
+					disabled={false}
 				>
 					<p>children</p>
 				</SearchToolDropdown>
 			);
+			const flatButton = wrapper.find(FlatButton);
 			chai.assert(wrapper.hasClass('open'));
-			chai.assert.ok(wrapper.find(FlatButton), 'should contain the Flatbutton child');
-			chai.assert.equal(wrapper.find(FlatButton).node.props.label, 'TestDropdown');
+			chai.assert.ok(flatButton, 'should contain the Flatbutton child');
+			chai.assert.equal(flatButton.node.props.label, 'TestDropdown');
 			chai.assert.ok(wrapper.find('p'), 'should render the children(<p> in this case)');
 			chai.assert.equal(wrapper.find('p').text(), 'children');
 		});
 
 		it('should render with dropdown disabled', () => {
+			const toggle = sinon.spy();
 			const wrapper = shallow(
 				<SearchToolDropdown
 					name="TestDropdown"
+					toggle={toggle}
+					open={false}
 					disabled
 				>
 					<p>children</p>
 				</SearchToolDropdown>
 			);
-			chai.assert.ok(wrapper.find(FlatButton), 'should contain the Flatbutton child');
-			chai.assert.equal(wrapper.find(FlatButton).node.props.label, 'TestDropdown');
-			chai.assert.equal(wrapper.find(FlatButton).node.props.disabled, true);
-			chai.assert.include(wrapper.find(FlatButton).node.props.className, 'search-tool-disabled');
+			const flatButton = wrapper.find(FlatButton);
+			chai.assert.ok(flatButton, 'should contain the Flatbutton child');
+			chai.assert.equal(flatButton.node.props.label, 'TestDropdown');
+			chai.assert.equal(flatButton.node.props.disabled, true);
+			chai.assert.include(flatButton.node.props.className, 'search-tool-disabled');
+			chai.assert.ok(wrapper.find('p'), 'should render the children(<p> in this case)');
+			chai.assert.equal(wrapper.find('p').text(), 'children');
+		});
+		it('should call toggle function with correct values', () => {
+			const toggle = sinon.spy();
+			const wrapper = shallow(
+				<SearchToolDropdown
+					name="TestDropdown"
+					toggle={toggle}
+					open={false}
+					disabled={false}
+				>
+					<p>children</p>
+				</SearchToolDropdown>
+			);
+			const flatButton = wrapper.find(FlatButton);
+			flatButton.simulate('click');
+			sinon.assert.calledWith(toggle, 'TestDropdown');
+			chai.assert.ok(flatButton, 'should contain the Flatbutton child');
+			chai.assert.equal(flatButton.node.props.label, 'TestDropdown');
 			chai.assert.ok(wrapper.find('p'), 'should render the children(<p> in this case)');
 			chai.assert.equal(wrapper.find('p').text(), 'children');
 		});

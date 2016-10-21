17458edb51a7c20a85f5dae80dabed1ca4a0ad12
Luke Hollis <lukehollis@gmail.com>
2016-10-21 19:24:41 -0400

    move commenter profile picture up the page in the manner of user profile pages

---
+++ client/views/commenters/CommenterVisualization.jsx
@@ -0,0 +1,48 @@
+
+import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+CommenterVisualization = React.createClass({
+
+	getChildContext() {
+		return { muiTheme: getMuiTheme(baseTheme) };
+	},
+
+	childContextTypes: {
+		muiTheme: React.PropTypes.object.isRequired,
+	},
+
+	mixins: [ReactMeteorData],
+
+	getMeteorData(){
+		let query = {};
+		return {
+			works: Works.find(query, {sort:{order:1}}).fetch(),
+		};
+	},
+
+
+	renderWorks(){
+		if(this.data.works.length === 3) {
+			return this.data.works.map((work, i) => {
+				return <WorkVisualization
+								key={i}
+								work={work} />;
+
+			});
+		}
+
+	},
+
+	render(){
+
+		return(
+			<div>
+				{this.renderWorks()}
+			</div>
+		);
+	}
+
+});

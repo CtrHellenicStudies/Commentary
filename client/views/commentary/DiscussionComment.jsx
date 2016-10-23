31d8d02819efb6844feadba29d53314e3e899457
Luke Hollis <lukehollis@gmail.com>
2016-10-23 02:04:22 -0400

    subscribe to discussion comments in discussion thread component

--- client/views/commentary/DiscussionComment.jsx
+++ client/views/commentary/DiscussionComment.jsx
@@ -7,254 +7,240 @@ DiscussionComment = React.createClass({
 
 	propTypes: {
 		discussionComment: React.PropTypes.object.isRequired,
-		currentUser: React.PropTypes.object
+		currentUser: React.PropTypes.object,
 	},
 
-	getInitialState(){
+	getInitialState() {
 		return {
-			editMode: false
-		}
-
+			editMode: false,
+		};
 	},
 
-	showEditMode(){
+	showEditMode() {
 		this.setState({
-			editMode: true
+			editMode: true,
 		});
-
 	},
 
-	closeEditMode(){
+	closeEditMode() {
 		this.setState({
-			editMode: false
+			editMode: false,
 		});
-
 	},
 
-	updateDiscussionComment(){
+	updateDiscussionComment() {
+		const content = $(this.refs.updateCommentForm).find('textarea').val();
 
-		var content = $(this.refs.updateCommentForm).find("textarea").val();
-
-		Meteor.call("discussionComments.update", {
+		Meteor.call('discussionComments.update', {
 			_id: this.props.discussionComment._id,
-			content: content
+			content,
 		});
 
 		this.setState({
-			editMode: false
+			editMode: false,
 		});
-
 	},
 
-	upvoteDiscussionComment(){
-		if(typeof this.props.currentUser !== "undefined"){
-			Meteor.call("discussionComments.upvote",
+	upvoteDiscussionComment() {
+		if (typeof this.props.currentUser !== 'undefined') {
+			Meteor.call('discussionComments.upvote',
 				this.props.discussionComment._id
 			);
-
 		}
-
 	},
 
 	render() {
-		var self = this;
-		var userIsLoggedIn = this.props.currentUser ? true : false;
-		var discussionComment = this.props.discussionComment;
+		const self = this;
+		const userIsLoggedIn = Meteor.user();
+		const discussionComment = this.props.discussionComment;
 		discussionComment.children = [];
-		var userUpvoted = false;
+		let userUpvoted = false;
+		let username = '';
 
-		var username = "";
-
-		if(discussionComment.user.emails.length){
-			username = discussionComment.user.emails[0].address.split("@")[0];
+		if (discussionComment.user.emails.length) {
+			username = discussionComment.user.emails[0].address.split('@')[0];
 		}
 
-		if(typeof this.props.currentUser !== "undefined" && discussionComment.voters.indexOf(this.props.currentUser._id) >= 0){
-				userUpvoted = true;
+		if (
+				typeof this.props.currentUser !== 'undefined'
+			&& discussionComment.voters.indexOf(this.props.currentUser._id) >= 0
+		) {
+			userUpvoted = true;
 		}
 
 		return (<div
-							className="discussion-comment paper-shadow"
+			className="discussion-comment paper-shadow"
+		>
+			<div
+				className="inner-comment-row"
+			>
+
+				<div className="discussion-commenter-profile-picture profile-picture paper-shadow">
+					<img src="/images/default_user.jpg" alt={username} />
+				</div>
+
+				<div className="discussion-commenter-meta">
+					<span className="discussion-commenter-name">
+						{username}
+					</span>
+					<span className="discussion-comment-date">
+						<span >Updated: </span>
+						{moment(discussionComment.updated).format('D MMMM YYYY')
+						|| moment(discussionComment.created).format('D MMMM YYYY')}
+					</span>
+				</div>
+
+			</div>
+			<div className="inner-comment-row">
+				<div className="discussion-comment-text">
+						{/* <div
+							dangerouslySetInnerHTML={{ __html: discussionComment.content}}
+							></div> */}
+						{this.state.editMode ?
+							<form
+								className="update-comment-form clearfix"
+								name="update-comment-form"
+								ref="updateCommentForm"
 							>
-								<div
-									className="inner-comment-row">
-
-										<div className="discussion-commenter-profile-picture profile-picture paper-shadow">
-												<img src="/images/default_user.jpg"/>
-										</div>
-
-										<div className="discussion-commenter-meta">
-												<span className="discussion-commenter-name">
-														{username}
-												</span>
-												<span className="discussion-comment-date">
-														<span >Updated: </span>
-														{moment(discussionComment.updated).format('D MMMM YYYY') || moment(discussionComment.created).format('D MMMM YYYY')}
-												</span>
-										</div>
-
+								<textarea
+									className="new-comment-text"
+									defaultValue={this.props.discussionComment.content}
+								/>
+								<div className="comment-edit-buttons">
+									<RaisedButton
+										label="Update"
+										className="submit-comment-button paper-shadow"
+										onClick={this.updateDiscussionComment}
+									/>
+									<FlatButton
+										label="Close"
+										className="close-form-button"
+										onClick={this.closeEditMode}
+									/>
 								</div>
-								<div className="inner-comment-row">
-										<div className="discussion-comment-text">
+							</form>
+						:
+							<div>{discussionComment.content}</div>
 
-												{/*<div
-													dangerouslySetInnerHTML={{ __html: discussionComment.content}}
-													></div>*/}
-												{this.state.editMode ?
-													<form className="update-comment-form clearfix" name="update-comment-form" ref="updateCommentForm">
-														<textarea className="new-comment-text" defaultValue={this.props.discussionComment.content}></textarea>
-														<div className="comment-edit-buttons">
-															<RaisedButton
-																label="Update"
-																className="submit-comment-button paper-shadow"
-																onClick={this.updateDiscussionComment}
-																>
-															</RaisedButton>
-															<FlatButton
-																label="Close "
-																className="close-form-button "
-																onClick={this.closeEditMode}>
-															</FlatButton>
-														</div>
-													</form>
-												:
-												<div>{discussionComment.content}</div>
+					}
 
-											}
+				</div>
+			</div>
+			<div className="inner-comment-row">
+				<FlatButton
+					label={discussionComment.votes}
+					onClick={this.upvoteDiscussionComment}
+					className={(userUpvoted) ? 'vote-up upvoted' : 'vote-up'}
+					icon={<FontIcon className="mdi mdi-chevron-up" />}
+				>
+					{userIsLoggedIn ?
+						''
+						:
+						<span className="vote-up-tooltip">
+							You must be signed in to upvote.
+						</span>
+					}
+				</FlatButton>
 
-										</div>
-								</div>
-								<div className="inner-comment-row" >
-										<FlatButton
-											label={discussionComment.votes}
-											onClick={this.upvoteDiscussionComment}
-											className={(userUpvoted) ? "vote-up upvoted" : "vote-up"}
-											icon={<FontIcon className="mdi mdi-chevron-up" />}
-											>
-											{userIsLoggedIn ?
-												""
-												:
-												<span className="vote-up-tooltip">
-													You must be signed in to upvote.
-												</span>
-											}
-										</FlatButton>
-										{/*<FlatButton
-											label="Reply"
-											onClick={this.showReplyForm}
-											className="reply">
-										</FlatButton>*/}
-										{/*<!--RaisedButton label="Share" className="share" onClick="share_discussionComment($event)">
-												Share
-										</RaisedButton-->*/}
-										{("currentUser" in self.props && self.props.currentUser !== null && typeof self.props.currentUser !== "undefined")
-											? (self.props.currentUser._id === discussionComment.user._id) ?
-												<FlatButton
-													label="Edit"
-													onClick={this.showEditMode}
-													className="edit"
-													>
-												</FlatButton>
-												: ""
-											: ""
-										}
-										{/*<FlatButton
-											label="Remove"
-											onClick={this.removeDiscussionComment}
-											className="remove"
-											>
-										</FlatButton>*/}
-								</div>
+				{('currentUser' in self.props && self.props.currentUser !== null && typeof self.props.currentUser !== 'undefined')
+					? (self.props.currentUser._id === discussionComment.user._id) ?
+						<FlatButton
+							label="Edit"
+							onClick={this.showEditMode}
+							className="edit"
+						/>
+						: ''
+					: ''
+				}
+			</div>
 
 
-								{false ?
-									<div className="reply-create-form" >
-											<div className="add-comment-wrap">
-													<form className="new-comment-form" name="new-comment-form" >
-															<div className="add-comment-row-1" >
-																	{/*<!--div className="profile-picture paper-shadow">
-																			<asset:image src="default_user.jpg"/>
-																	</div-->*/}
-																	<textarea className="new-comment-text" placeholder="Enter your reply here . . . " enter-submit="add_discussion_reply($event)">
-																	</textarea>
-																	<RaisedButton label="Submit" type="submit" className="submit-comment-button paper-shadow" >Submit</RaisedButton>
-																	<RaisedButton label="Close Reply"
-																						 className="close-form-button " onClick={this.closeReply}>Close
-																	</RaisedButton>
-															</div>
-													</form>
-											</div>
-									</div>
-								: ""}
+			{false ?
+				<div className="reply-create-form" >
+					<div className="add-comment-wrap">
+						<form
+							className="new-comment-form"
+							name="new-comment-form"
+						>
+							<div className="add-comment-row-1" >
+								<textarea
+									className="new-comment-text"
+									placeholder="Enter your reply here . . . "
+								/>
+								<RaisedButton
+									label="Submit"
+									type="submit"
+									className="submit-comment-button paper-shadow"
+								/>
+								<RaisedButton
+									label="Close Reply"
+									className="close-form-button"
+									onClick={this.closeReply}
+								/>
+							</div>
+						</form>
+					</div>
+				</div>
+			: ''}
 
-								<div className="discussion-comment-children">
+			<div className="discussion-comment-children">
 
-									{discussionComment.children.map(function(discussionCommentChild, j){
-										<div
-											key={j}
-											className="discussion-comment discussion-comment-child"
-											 >
-												<div className="inner-comment-row">
-														<div className="discussion-commenter-profile-picture profile-picture paper-shadow">
-																<img src="/images/default_user.png"/>
-														</div>
-														<div className="discussion-commenter-meta">
-																<span className="discussion-commenter-name">
-																		{discussionCommentChild.user.name}
-																</span>
-																<span className="discussion-comment-date">
-																		{discussionCommentChild.updated}
-																</span>
-														</div>
+				{discussionComment.children.map((discussionCommentChild, j) =>
+					<div
+						key={j}
+						className="discussion-comment discussion-comment-child"
+					>
+						<div className="inner-comment-row">
+							<div className="discussion-commenter-profile-picture profile-picture paper-shadow">
+								<img src="/images/default_user.png" alt={username} />
+							</div>
+							<div className="discussion-commenter-meta">
+								<span className="discussion-commenter-name">
+									{discussionCommentChild.user.name}
+								</span>
+								<span className="discussion-comment-date">
+									{discussionCommentChild.updated}
+								</span>
+							</div>
+						</div>
+						<div className="inner-comment-row">
+							<div className="discussion-comment-text">
+								<p
+									dangerouslySetInnerHTML={{
+										__html: discussionCommentChild.content,
+									}}
+								/>
+							</div>
+						</div>
+						<div className="inner-comment-row">
+							<FlatButton
+								label={discussionComment.votes}
+								onClick={this.upvoteDiscussionComment}
+								className="vote-up upvoted"
+								icon={<FontIcon className="mdi mdi-chevron-up" />}
+							/>
+							<FlatButton
+								label="Reply"
+								onClick={this.showReplyForm}
+								className="reply"
+							/>
+							<FlatButton
+								label="Edit"
+								onClick={this.editDiscussionComment}
+								className="edit"
+							/>
+							<FlatButton
+								label="Remove"
+								onClick={this.removeDiscussionComment}
+								className="remove"
+							/>
+						</div>
 
-												</div>
-												<div className="inner-comment-row">
-														<div className="discussion-comment-text">
-																<p dangerouslySetInnerHTML={{ __html: discussionCommentChild.content}}></p>
-														</div>
-												</div>
-												<div className="inner-comment-row">
-													<FlatButton
-														label={discussionComment.votes}
-														onClick={this.upvoteDiscussionComment}
-														className="vote-up upvoted"
-														icon={<FontIcon className="mdi mdi-chevron-up" />}
-														>
-													</FlatButton>
-													<FlatButton
-														label="Reply"
-														onClick={this.showReplyForm}
-														className="reply">
-													</FlatButton>
-													{/*<!--RaisedButton label="Share" className="share" onClick="share_discussionComment($event)">
-															Share
-													</RaisedButton-->*/}
-													<FlatButton
-														label="Edit"
-														onClick={this.editDiscussionComment}
-														className="edit"
-														>
-													</FlatButton>
-													<FlatButton
-														label="Remove"
-														onClick={this.removeDiscussionComment}
-														className="remove"
-														>
-													</FlatButton>
-
-												</div>
-
-
-
-										{/*<!-- .discussion-comment-child -->*/}</div>
-
-									})}
-
-								{/*<!-- .discussion-comment-children -->*/}</div>
-
-						{/*<!-- .discussion-comment -->*/}</div>
-
-		 );
-	 }
+					{/* <!-- .discussion-comment-child --> */}</div>
+				)}
+				{/* <!-- .discussion-comment-children --> */}</div>
+			{/* <!-- .discussion-comment --> */}</div>
+		);
+	},
 
 });
--- client/views/commentary/DiscussionThread.jsx
+++ client/views/commentary/DiscussionThread.jsx
@@ -1,181 +1,189 @@
 import IconButton from 'material-ui/IconButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
-import FontIcon from 'material-ui/FontIcon';
 
 DiscussionThread = React.createClass({
 
 	propTypes: {
 		comment: React.PropTypes.object.isRequired,
 		discussionVisible: React.PropTypes.bool.isRequired,
-		showDiscussionThread: React.PropTypes.func.isRequired
-	},
-
-	getInitialState(){
-		return {
-		}
-
+		showDiscussionThread: React.PropTypes.func.isRequired,
+		hideDiscussionThread: React.PropTypes.func.isRequired,
 	},
 
 	mixins: [ReactMeteorData],
 
-	getMeteorData(){
-		var query = {commentId: this.props.comment._id},
-				sort = {votes: -1, updated: -1};
-
+	getInitialState() {
 		return {
-			loaded : true,
-			currentUser: Meteor.users.findOne({_id: Meteor.userId()}),
-			discussionComments: DiscussionComments.find(query, {sort:sort}).fetch()
+			sortMethod: 'votes',
 		};
 	},
 
-	showDiscussionThread(){
-			this.props.showDiscussionThread(this.props.comment);
+	getMeteorData() {
+		let discussionComments = [];
+		let loaded = false;
+
+		const handle = Meteor.subscribe('discussionComments', this.props.comment._id);
+		if (handle.ready()) {
+			discussionComments = DiscussionComments.find({}).fetch();
+			loaded = true;
+		}
+
+		return {
+			currentUser: Meteor.users.findOne({ _id: Meteor.userId() }),
+			discussionComments,
+			loaded,
+		};
 	},
-	hideDiscussionThread(){
-			this.props.hideDiscussionThread();
+
+	showDiscussionThread() {
+		this.props.showDiscussionThread(this.props.comment);
 	},
 
-	addDiscussionComment(){
+	hideDiscussionThread() {
+		this.props.hideDiscussionThread();
+	},
 
-		var content = $(this.refs.newCommentForm).find("textarea").val();
+	addDiscussionComment() {
+		const content = $(this.refs.newCommentForm).find('textarea').val();
 
-		Meteor.call("discussionComments.insert", {
-		//DiscussionComments.insert({
-			content: content,
-			commentId: this.props.comment._id
+		Meteor.call('discussionComments.insert', {
+			content,
+			commentId: this.props.comment._id,
 		});
 
-		$(this.refs.newCommentForm).find("textarea").val("");
-
+		$(this.refs.newCommentForm).find('textarea').val('');
 	},
 
 	render() {
-		var self = this;
-		var userIsLoggedIn = this.data.currentUser ? true : false;
+		const self = this;
+		const userIsLoggedIn = Meteor.user();
 
-		var discussionWrapClass = "discussion-wrap";
+		let discussionWrapClass = 'discussion-wrap';
 
-		if(this.state.discussionVisible){
-			discussionWrapClass += " discussion-visible";
-
+		if (this.state.discussionVisible) {
+			discussionWrapClass += ' discussion-visible';
 		}
 
-		var textareaPlaceholder = "";
-		if(userIsLoggedIn){
-			textareaPlaceholder = "Enter your comment here . . .";
-		}else {
-			textareaPlaceholder = "Please login to enter a comment.";
+		let textareaPlaceholder = '';
+		if (userIsLoggedIn) {
+			textareaPlaceholder = 'Enter your comment here . . .';
+		} else {
+			textareaPlaceholder = 'Please login to enter a comment.';
 		}
 
-		//console.log("CommentDiscussion.data", this.data);
+		// console.log("CommentDiscussion.data", this.data);
 
 		return (
 
-				<div className={discussionWrapClass} >
-						<div
-							onClick={this.showDiscussionThread}
-							className="continue-discussion">
-								<i className="mdi mdi-comment"></i>
-								{this.data.discussionComments.length ?
-										<span className="continue-discussion-text">
-											{this.data.discussionComments.length}
-										</span>
-									: ""
-								}
-						</div>
-
-						{!this.data.loaded ?
-							<div className="ahcip-spinner" >
-									<div className="double-bounce1"></div>
-									<div className="double-bounce2"></div>
-
-							</div>
-						: ""
-						}
-
-						<div className="discussion-thread" >
-								<div className="add-comment-wrap paper-shadow " >
-									<IconButton
-											className="close-discussion paper-shadow"
-											iconClassName="mdi mdi-close"
-											onClick={this.hideDiscussionThread}
-										/>
-
-									<form ref="newCommentForm" className="new-comment-form" name="new-comment-form">
-											<div className="add-comment-row-1" >
-													<div className="profile-picture paper-shadow">
-															<img src="/images/default_user.jpg"/>
-													</div>
-													<textarea className="new-comment-text"
-																		name="newCommentText"
-																		placeholder={textareaPlaceholder} >
-													</textarea>
-											</div>
-											<div className="add-comment-row-2 add-comment-row">
-													<div className="error-message">
-															<span className="error-message-text">Please enter your text to submit.</span>
-													</div>
-													{ userIsLoggedIn ?
-															<RaisedButton
-																label="Submit"
-																className="submit-comment-button paper-shadow"
-																onClick={this.addDiscussionComment} ></RaisedButton>
-														:
-															<div className="new-comment-login">
-																<FlatButton
-																	label="Join"
-																	className="join-link"
-																	href="/sign-up">
-																</FlatButton>
-																<FlatButton
-																	label="Login"
-																	className="login-link"
-																	href="/sign-in">
-																</FlatButton>
-															</div>
-														}
-											</div>
-									</form>
-								</div>
-								<div className="sort-by-wrap" >
-									{/*
-										<span className="sort-by-label">Sort by:</span>
-										<RaisedButton
-											label="Top"
-											className="sort-by-option selected-sort sort-by-top"
-											onClick={this.toggleSort}>
-										</RaisedButton>
-										<RaisedButton
-											label="Newest"
-											className="sort-by-option sort-by-new"
-											onClick={this.toggleSort}>
-										</RaisedButton>
-										*/}
-								</div>
-								{this.data.discussionComments.length === 0 ?
-									<div className="no-results-wrap" >
-											<span className="no-results-text">No discussion comments.</span>
-									</div>
-									: ""
-								}
-								{this.data.discussionComments.map(function(discussionComment, i){
-
-									return <DiscussionComment
-										key={i}
-										className="discussion-comment paper-shadow"
-										discussionComment={discussionComment}
-										currentUser={self.data.currentUser}
-										/>
-
-								})}
-
-						</div>{/*<!-- .discussion-thread -->*/}
-
+			<div className={discussionWrapClass} >
+				<div
+					onClick={this.showDiscussionThread}
+					className="continue-discussion"
+				>
+					<i className="mdi mdi-comment" />
+					{this.data.discussionComments.length ?
+						<span className="continue-discussion-text">
+							{this.data.discussionComments.length}
+						</span>
+					: ''}
 				</div>
 
-		 );
-	 }
+				{!this.data.loaded ?
+					<div className="ahcip-spinner" >
+						<div className="double-bounce1" />
+						<div className="double-bounce2" />
+					</div>
+				:
+					<div className="discussion-thread" >
+						<div className="add-comment-wrap paper-shadow " >
+							<IconButton
+								className="close-discussion paper-shadow"
+								iconClassName="mdi mdi-close"
+								onClick={this.hideDiscussionThread}
+							/>
 
+							<form
+								ref="newCommentForm"
+								className="new-comment-form"
+								name="new-comment-form"
+							>
+								<div className="add-comment-row-1" >
+									<div className="profile-picture paper-shadow">
+										<img
+											src="/images/default_user.jpg"
+											alt="Commentary User"
+										/>
+									</div>
+									<textarea
+										className="new-comment-text"
+										name="newCommentText"
+										placeholder={textareaPlaceholder}
+									/>
+								</div>
+								<div className="add-comment-row-2 add-comment-row">
+									<div className="error-message">
+										<span className="error-message-text">Please enter your text to submit.</span>
+									</div>
+									{ userIsLoggedIn ?
+										<RaisedButton
+											label="Submit"
+											className="submit-comment-button paper-shadow"
+											onClick={this.addDiscussionComment}
+										/>
+									:
+										<div
+											className="new-comment-login"
+										>
+											<FlatButton
+												label="Join"
+												className="join-link"
+												href="/sign-up"
+											/>
+											<FlatButton
+												label="Login"
+												className="login-link"
+												href="/sign-in"
+											/>
+										</div>
+									}
+								</div>
+							</form>
+						</div>
+						<div
+							className="sort-by-wrap"
+						>
+								{/*
+									<span className="sort-by-label">Sort by:</span>
+									<RaisedButton
+										label="Top"
+										className="sort-by-option selected-sort sort-by-top"
+										onClick={this.toggleSort}>
+									</RaisedButton>
+									<RaisedButton
+										label="Newest"
+										className="sort-by-option sort-by-new"
+										onClick={this.toggleSort}>
+									</RaisedButton>
+									*/}
+						</div>
+						{this.data.discussionComments.length === 0 ?
+							<div className="no-results-wrap" >
+								<span className="no-results-text">No discussion comments.</span>
+							</div>
+							: ''
+						}
+						{this.data.discussionComments.map((discussionComment, i) =>
+							<DiscussionComment
+								key={i}
+								className="discussion-comment paper-shadow"
+								discussionComment={discussionComment}
+								currentUser={self.data.currentUser}
+							/>
+						)}
+					</div>
+				}
+			</div>
+		);
+	},
 });

6618262d27771783fcd6fca86b572d19bd86ee76
Luke Hollis <lukehollis@gmail.com>
2016-10-22 23:52:14 -0400

    lint login and signup modals

--- client/shared/ModalLogin.jsx
+++ client/shared/ModalLogin.jsx
@@ -1,61 +1,44 @@
-
-import RaisedButton from 'material-ui/RaisedButton';
-import FlatButton from 'material-ui/FlatButton';
-import FontIcon from 'material-ui/FontIcon';
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 ModalLogin = React.createClass({
 
-	getChildContext() {
-		return { muiTheme: getMuiTheme(baseTheme) };
+	propTypes: {
+		lowered: React.PropTypes.bool,
+		closeModal: React.PropTypes.func,
 	},
 
 	childContextTypes: {
 		muiTheme: React.PropTypes.object.isRequired,
 	},
 
-	propTypes: {
-		lowered: React.PropTypes.bool,
-		closeModal: React.PropTypes.func
+	getChildContext() {
+		return { muiTheme: getMuiTheme(baseTheme) };
 	},
 
-	render(){
+	render() {
+		const lowered = this.props.lowered;
 
-		let styles = {
-			flatButton : {
-				width: "auto",
-				minWidth: "none",
-				height: "55px",
-				padding: "10px 5px"
-			},
-			flatIconButton : {
-				padding: "10px 20px",
-				width: "auto",
-				minWidth: "none",
-				height: "55px",
-
-			}
-
-		};
-
-		let lowered = this.props.lowered;
-
-		return (<div>
-			{!Meteor.userId() ?
-				<div className={"ahcip-modal-login ahcip-modal ahcip-login-signup" + ((lowered) ? " lowered" : "")}>
-					<div className="close-modal paper-shadow"
-						onClick={this.props.closeModal}
+		return (
+			<div>
+				{!Meteor.userId() ?
+					<div
+						className={`ahcip-modal-login
+						ahcip-modal ahcip-login-signup ${((lowered) ? ' lowered' : '')}`}
+					>
+						<div
+							className="close-modal paper-shadow"
+							onClick={this.props.closeModal}
 						>
-						<i className="mdi mdi-close"></i>
+							<i className="mdi mdi-close" />
+						</div>
+						<div className="modal-inner">
+							<BlazeToReact blazeTemplate="atForm" state="signIn" />
+						</div>
 					</div>
-					<div className="modal-inner">
-						<BlazeToReact blazeTemplate="atForm" state="signIn" />
-					</div>
-				</div>
-				:
-				""
-			}
-			</div>);
-	}
+					: ''
+				}
+			</div>
+		);
+	},
 });

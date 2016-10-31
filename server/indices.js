d3b254a44bfdb19e0f06eb789d335c2a2a299c26
Jess Winter <jess@misclabs.com>
2016-10-31 16:21:54 -0400

    Add indices for keyword context queries

--- server/indices.js
+++ server/indices.js
@@ -1,3 +1,13 @@
 Comments._ensureIndex({
 	'$**': 'text',
 });
+
+Comments._ensureIndex({
+	'keywords._id': 1,
+});
+
+TextNodes._ensureIndex({
+	"work.slug": 1,
+	"subwork.n": 1,
+	"text.n": 1
+});

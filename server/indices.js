2f51f0821877c12403c4f91acf01a89298e5c48e
Jess Winter <jess@misclabs.com>
2016-11-02 12:27:03 -0400

    Add indices for sorted homepage queries on comments and commenters.

--- server/indices.js
+++ server/indices.js
@@ -3,6 +3,13 @@ Comments._ensureIndex({
 });
 
 Comments._ensureIndex({
+	'work.order': 1,
+	'subwork.n': 1,
+	'lineFrom': 1,
+	'nLines': -1
+});
+
+Comments._ensureIndex({
 	'keywords._id': 1,
 });
 
@@ -10,4 +17,8 @@ TextNodes._ensureIndex({
 	"work.slug": 1,
 	"subwork.n": 1,
 	"text.n": 1
+});
+
+Commenters._ensureIndex({
+	name:1
 });

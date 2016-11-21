9719f72bc209353c3b8ca0fbb91771b48b0e4296
jess <jess@misclabs.com>
2016-11-21 16:01:15 -0500

    Display build tag in console.

--- client/startup.js
+++ client/startup.js
@@ -1,3 +1,1 @@
 import '../imports/avatar/client/avatar_client_startup.js';
-
-console.log('Hello, Jenkins. (client)');
--- server/startup.js
+++ server/startup.js
@@ -1,4 +1,2 @@
 import '../imports/startup/server/register_api.js';
 import '../imports/avatar/server/avatar_server_startup.js';
-
-console.log('Hello, Jenkins. (server)');

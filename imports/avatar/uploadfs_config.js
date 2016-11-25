97b60ddf1f498e11acf157ea9f17b66fad66fffb
jess <jess@misclabs.com>
2016-11-25 15:54:55 -0500

    Update UFS deps

--- imports/avatar/uploadfs_config.js
+++ imports/avatar/uploadfs_config.js
@@ -1,17 +1,5 @@
 import {UploadFS} from 'meteor/jalik:ufs';
 
-// TODO: this should probably be a local per project file
-
-// Activate simulation for slowing file reading
-// UploadFS.config.simulateReadDelay = 1000; // 1 sec
-
-// Activate simulation for slowing file uploading
-// UploadFS.config.simulateUploadSpeed = 128000; // 128kb/s
-
-// Activate simulation for slowing file writing
-// UploadFS.config.simulateWriteDelay = 2000; // 2 sec
-UploadFS.config.simulateWriteDelay = 0;
-
 // This path will be appended to the site URL, be sure to not put a "/" as first character
 // for example, a PNG file with the _id 12345 in the "photos" store will be available via this URL :
 // http://www.yourdomain.com/uploads/photos/12345.png
@@ -19,7 +7,7 @@ UploadFS.config.storesPath = 'var';
 
 // Set the temporary directory where uploading files will be saved
 // before sent to the store.
-//UploadFS.config.tmpDir = '/tmp/uploads';
+UploadFS.config.tmpDir = '/tmp/uploads';
 
 // Set the temporary directory permissions.
-//UploadFS.config.tmpDirPermissions = '0700';
+UploadFS.config.tmpDirPermissions = '0700';

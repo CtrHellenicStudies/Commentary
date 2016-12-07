41282b1b805b56c14294d2a4eb19580830ed7513
Suhaib Khan <suheb.work@gmail.com>
2016-12-08 03:31:28 +0530

    Adding slider to show recent comments
    Signed-off-by: Suhaib Khan <suheb.work@gmail.com>

--- client/views/commentary/CommentsRecent.jsx
+++ client/views/commentary/CommentsRecent.jsx
@@ -1,5 +1,6 @@
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Slider from 'react-slick';
 
 CommentsRecent = React.createClass({
 
@@ -25,6 +26,14 @@ CommentsRecent = React.createClass({
 	},
 
 	render() {
+		const settings = {
+			dots: true,
+			infinite: true,
+			autoplay: true,
+			speed: 500,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+		};
 		return (
 			<section className="background-gray recent-comments">
 
@@ -35,30 +44,28 @@ CommentsRecent = React.createClass({
 								Recently from the Commentary
 							</h3>
 							<i className="mdi mdi-format-quote quote-icon" />
-							<div className="text-slider slider-arrow-controls">
-								<ul className="slides">
-									{this.data.recentComments.map((comment, index) => (
-										<li
-											key={index}
-										>
-											<p
-												dangerouslySetInnerHTML={{
-													__html: comment.revisions[comment.revisions.length - 1].text,
-												}}
-											/>
-											<h4>
-												-
-												{comment.commenters.map((commenter) => (
-													` ${commenter.name},`
-												))}
-												{` ${comment.work.title} ${
-												comment.subwork.title}.${comment.lineFrom}-${
-												comment.lineFrom + comment.nLines}`}
-											</h4>
-										</li>
-									))}
-								</ul>
-							</div>
+							<Slider {...settings}>
+								{this.data.recentComments.map((comment, index) => (
+									<div
+										key={index}
+									>
+										<p
+											dangerouslySetInnerHTML={{
+												__html: comment.revisions[comment.revisions.length - 1].text,
+											}}
+										/>
+										<h4>
+											-
+											{comment.commenters.map((commenter) => (
+												` ${commenter.name},`
+											))}
+											{` ${comment.work.title} ${
+											comment.subwork.title}.${comment.lineFrom}-${
+											comment.lineFrom + comment.nLines}`}
+										</h4>
+									</div>
+								))}
+							</Slider>
 						</div>
 					</div>
 				</div>

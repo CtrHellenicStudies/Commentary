8209ff6b862fdec70af604842a66da1109f743a9
Arslan Nasir <arslan@mobylogix.com>
2017-02-28 16:18:14 +0500

    Settings schema updated and insert method added

---
+++ server/methods/settings.js
@@ -0,0 +1,22 @@
+import { Meteor } from 'meteor/meteor';
+
+import { Settings } from '/imports/collections/settings.js';
+
+Meteor.methods({
+  'settings.insert'(data) {
+    check(data, Object);
+
+    return Settings.insert({
+      name: data.name,
+      domain: data.domain,
+      title: data.title,
+      subtitle: data.subtitle,
+      footer: data.footer,
+      emails: {
+        from: data.fromEmail,
+        contact: data.contactEmail
+      },
+      tenantId: data.tenantId
+    });
+  }
+});

254e5c2310b1f9e4cb6a185763a4436008e89e8a
Luke Hollis <lukehollis@gmail.com>
2017-04-07 18:02:49 -0400

    login with a token generated on signup and save it in a cookie

--- server/smtp.js
+++ server/smtp.js
@@ -1,18 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
 if (Meteor.isServer) {
-	console.log('mail_URL', process.env.MAIL_URL);
 	if ('smtp' in Meteor.settings.private) {
 		process.env.MAIL_URL = Meteor.settings.private.smtp;
-		console.log('mail_URL', process.env.MAIL_URL);
-
-		/*
-		Email.send({
-			to: 'lukehollis@gmail.com',
-			from: 'no-reply@ahcip.chs.harvard.edu',
-			subject: 'testing',
-			text: 'foobar'
-		});
-		*/
 	}
 }

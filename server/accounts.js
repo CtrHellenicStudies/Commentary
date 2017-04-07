248206c04c01fcc6181767d8dbed126ebdebc126
Luke Hollis <lukehollis@gmail.com>
2017-04-07 15:24:14 -0400

    update global config options integration with sending verification emails

--- server/accounts.js
+++ server/accounts.js
@@ -2,14 +2,20 @@
  * Account email template
  */
 
-Accounts.emailTemplates.siteName = 'A Homer Commentary in Progress';
-Accounts.emailTemplates.from = 'A Homer Commentary in Progress <no-reply@ahcip.archimedes.digital>';
+Accounts.emailTemplates.siteName = Config.title();
+Accounts.emailTemplates.from = `${Config.title()} <no-reply@ahcip.chs.harvard.edu>`;
 Accounts.emailTemplates.verifyEmail = {
 	subject() {
 		return 'Account Registration Confirmation';
 	},
 	text(user, url) {
-		return `Dear ${user.username},
+		let username = 'Commentary User';
+
+		if ('username' in user) {
+			username = user.username;
+		}
+
+		return `Dear ${username},
 
 			Please click the link below to verify your email account.
 
@@ -22,7 +28,7 @@ Accounts.emailTemplates.verifyEmail = {
 	html(user, url) {
 		SSR.compileTemplate('verifyEmail', Assets.getText('verify_email.html'));
 
-		const html = SSR.render('verifyEmail', { url });
+		const html = SSR.render('verifyEmail', { url, user });
 		return html;
 	},
 };

cab9dbe1477978a33b40f44a9d2aa9a363a06686
Luke Hollis <lukehollis@gmail.com>
2017-04-07 15:23:38 -0400

    update method of setting process env MAIL_URL

--- server/smtp.js
+++ server/smtp.js
@@ -1,3 +1,18 @@
-if ('smtp' in Meteor.settings.private) {
-	process.env.MAIL_URL = Meteor.settings.private.smtp;
+import { Meteor } from 'meteor/meteor';
+
+if (Meteor.isServer) {
+	console.log('mail_URL', process.env.MAIL_URL);
+	if ('smtp' in Meteor.settings.private) {
+		process.env.MAIL_URL = Meteor.settings.private.smtp;
+		console.log('mail_URL', process.env.MAIL_URL);
+
+		/*
+		Email.send({
+			to: 'lukehollis@gmail.com',
+			from: 'no-reply@ahcip.chs.harvard.edu',
+			subject: 'testing',
+			text: 'foobar'
+		});
+		*/
+	}
 }

0ab84ad770b6987fca308e9939472ef63617aadd
Michał Pierzchlewicz <kontakt@michalpierzchlewicz.pl>
2017-05-08 16:36:28 +0200

    refactor CommenterReferenceWorks

--- imports/ui/components/commenters/CommenterReferenceWorks/CommenterReferenceWorks.js
+++ imports/ui/components/commenters/CommenterReferenceWorks/CommenterReferenceWorks.js
@@ -2,35 +2,25 @@ import React from 'react';
 
 import ReferenceWorksList from '/imports/ui/components/referenceWorks/ReferenceWorksList';
 
-const CommenterReferenceWorks = React.createClass({
-
-	propTypes: {
-		commenter: React.PropTypes.object.isRequired,
-	},
-
-	render() {
-		const { commenter } = this.props;
-		if (!commenter) {
-			return null;
-		}
-		
-		return (
-			<div
-				className="commenter-reference-works"
-			>
-				<div className="commenter-reference-works-title">
-					<h2>
-						Reference Works
-					</h2>
-				</div>
-				<ReferenceWorksList
-					commenterId={commenter._id}
-				/>
-			</div>
-		);
-	},
-
-});
+const CommenterReferenceWorks = ({ commenter }) => (
+	<div
+		className="commenter-reference-works"
+	>
+		<div className="commenter-reference-works-title">
+			<h2>
+				Reference Works
+			</h2>
+		</div>
+		<ReferenceWorksList
+			commenterId={commenter._id}
+		/>
+	</div>
+);
+CommenterReferenceWorks.propTypes = {
+	commenter: React.PropTypes.shape({
+		_id: React.PropTypes.string.isRequired,
+	}).isRequired,
+};
 
 
 export default CommenterReferenceWorks;

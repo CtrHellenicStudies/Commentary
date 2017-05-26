f61f6cccca770be3afe35cbec703fdcb75ea963f
Luke Hollis <lukehollis@gmail.com>
2017-05-26 00:31:31 -0400

    add functionality to load more button on the recent activity list

--- imports/ui/components/community/RecentActivity/RecentActivity.js
+++ imports/ui/components/community/RecentActivity/RecentActivity.js
@@ -9,12 +9,32 @@ import RecentActivityList from '../RecentActivityList';
 
 class RecentActivity extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			skip: 0,
+			limit: 12,
+		};
+	}
+
+	loadMore() {
+		this.setState({
+			limit: this.state.limit + 12,
+		});
+	}
+
 	render() {
+		const { skip, limit } = this.state;
+
 		return (
 			<div className="recentActivity">
 				<h3>Recent Community Activity</h3>
 				<MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
-					<RecentActivityList />
+					<RecentActivityList
+						skip={skip}
+						limit={limit}
+						loadMore={this.loadMore.bind(this)}
+					/>
 				</MuiThemeProvider>
 			</div>
 		);

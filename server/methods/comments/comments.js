fdecd325181d85b9ffe8a9e422a1c2f939b66d01
Michał Pierzchlewicz <kontakt@michalpierzchlewicz.pl>
2017-06-06 16:54:44 +0200

    meteor methods tests: comments

--- server/methods/comments/comments.js
+++ server/methods/comments/comments.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { check, Match } from 'meteor/check';
 import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
+import { Random } from 'meteor/random';
 
 import Comments from '/imports/api/collections/comments';
 import Commenters from '/imports/api/collections/commenters';
@@ -10,15 +11,12 @@ import Commenters from '/imports/api/collections/commenters';
 const commentsInsert = (token, comment) => {
 	check(token, String);
 	check(comment, Object);
+
 	const roles = ['editor', 'admin', 'commenter'];
-	if ((
-			!Meteor.userId()
-			&& !Roles.userIsInRole(Meteor.user(), roles)
-		)
-		&& !Meteor.users.findOne({
-			roles: roles,
-			'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(token),
-		})
+	if (!Meteor.users.findOne({
+		roles: { $elemMatch: { $in: roles } },
+		'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(token),
+	})
 	) {
 		throw new Meteor.Error('comment-insert', 'not-authorized');
 	}
@@ -31,28 +29,31 @@ const commentsInsert = (token, comment) => {
 	}
 
 	return commentId;
-}
+};
 
 const commentsUpdate = (token, commentId, update) => {
 	check(token, Match.Maybe(String));
 	check(commentId, String);
 	check(update, Object);
 
-	const user = Meteor.user() || Meteor.users.findOne({
+	const roles = ['editor', 'admin', 'commenter'];
+
+	const user = Meteor.users.findOne({
+		roles: { $elemMatch: { $in: roles } },
 		'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken((token || '')),
 	});
 
-	const comment = Comments.find({ _id: commentId }).fetch()[0];
-	const commenters = Commenters.find().fetch();
-
-	if (!Roles.userIsInRole(user, ['editor', 'admin', 'commenter'])) {
-		throw new Meteor.Error(`User ${user._id} attempted to update comment but was in an unauthorized role`);
+	if (!user) {
+		throw new Meteor.Error('comment-update', 'not-authorized');
 	}
 
+	const comment = Comments.findOne({ _id: commentId });
+	const commenters = Commenters.find().fetch();
+
 	let allowedToEdit = false;
-	user.canEditCommenters.forEach(commenterId => {
-		comment.commenters.forEach(commenter => {
-			commenters.forEach(_commenter => {
+	user.canEditCommenters.forEach((commenterId) => {
+		comment.commenters.forEach((commenter) => {
+			commenters.forEach((_commenter) => {
 				if (
 						commenter.slug === _commenter.slug
 					&& _commenter._id === commenterId
@@ -64,7 +65,7 @@ const commentsUpdate = (token, commentId, update) => {
 	});
 
 	if (!allowedToEdit) {
-		throw new Meteor.Error(`User ${user._id} attempted to update comment but was unauthorized on the commenter`);
+		throw new Meteor.Error('comment-update', 'not-authorized');
 	}
 
 	try {
@@ -74,22 +75,17 @@ const commentsUpdate = (token, commentId, update) => {
 	}
 
 	return commentId;
-}
+};
 
 const commentsRemove = (token, commentId) => {
 	check(token, String);
 	check(commentId, String);
 
 	const roles = ['editor', 'admin', 'commenter'];
-	if ((
-			!Meteor.userId()
-			&& !Roles.userIsInRole(Meteor.user(), roles)
-		)
-		&& !Meteor.users.findOne({
-			roles: 'admin',
-			'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(token),
-		})
-	) {
+	if (!Meteor.users.findOne({
+		roles: { $elemMatch: { $in: roles } },
+		'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(token),
+	})) {
 		throw new Meteor.Error('comment-delete', 'not-authorized');
 	}
 
@@ -100,23 +96,31 @@ const commentsRemove = (token, commentId) => {
 	}
 
 	return commentId;
-}
+};
 
 const commentsAddRevision = (commentId, revision) => {
 	check(commentId, String);
 	check(revision, Object);
 
-	const comment = Comments.find({ _id: commentId }).fetch()[0];
+	const comment = Comments.findOne({ _id: commentId });
 	const commenters = Commenters.find().fetch();
 
-	if (!Roles.userIsInRole(Meteor.user(), ['editor', 'admin', 'commenter'])) {
-		throw new Meteor.Error(`User ${Meteor.user()._id} attempted to add revision but was in an unauthorized role`);
+	const roles = ['editor', 'admin', 'commenter'];
+
+	const user = Meteor.user();
+
+	if (!user) {
+		throw new Meteor.Error('comment-update', 'not-authorized');
+	}
+
+	if (!Roles.userIsInRole(user, roles)) {
+		throw new Meteor.Error(`User ${user._id} attempted to add revision but was in an unauthorized role`);
 	}
 
 	let allowedToEdit = false;
-	Meteor.user().canEditCommenters.forEach(commenterId => {
-		comment.commenters.forEach(commenter => {
-			commenters.forEach(_commenter => {
+	user.canEditCommenters.forEach((commenterId) => {
+		comment.commenters.forEach((commenter) => {
+			commenters.forEach((_commenter) => {
 				if (
 						commenter.slug === _commenter.slug
 					&& _commenter._id === commenterId
@@ -128,11 +132,11 @@ const commentsAddRevision = (commentId, revision) => {
 	});
 
 	if (!allowedToEdit) {
-		throw new Meteor.Error(`User ${Meteor.user()._id} attempted to add revision but was unauthorized on the commenter`);
+		throw new Meteor.Error(`User ${user._id} attempted to add revision but was unauthorized on the commenter`);
 	}
 
-	const revisionId = new Meteor.Collection.ObjectID();
-	revision._id = revisionId.valueOf();
+	const revisionId = Random.id();
+	revision._id = revisionId;
 
 	try {
 		Comments.update({
@@ -145,15 +149,23 @@ const commentsAddRevision = (commentId, revision) => {
 	} catch (err) {
 		throw new Meteor.Error(`Error adding revision to comment: ${err}`);
 	}
-}
+	return revisionId;
+};
 
 const commentsRemoveRevision = (commentId, revision) => {
 	check(commentId, String);
 	check(revision, Object);
+
 	const roles = ['editor', 'admin'];
 
-	if (!Roles.userIsInRole(Meteor.user(), roles)) {
-		throw new Meteor.Error(`Permission denied on method comment.remove.revision for user ${Meteor.userId()}`);
+	const user = Meteor.user();
+
+	if (!user) {
+		throw new Meteor.Error('comment-update', 'not-authorized');
+	}
+
+	if (!Roles.userIsInRole(user, roles)) {
+		throw new Meteor.Error(`User ${user._id} attempted to remove revision but was in an unauthorized role`);
 	}
 
 	try {
@@ -167,32 +179,28 @@ const commentsRemoveRevision = (commentId, revision) => {
 			getAutoValues: false,
 		});
 	} catch (err) {
-		throw new Meteor.Error(`Error remove comment revision: ${err}`);
+		throw new Meteor.Error('comment-remove-revision', err);
 	}
-}
+};
 
 const commentsGetSuggestions = (value) => {
 	check(value, String);
 
 	if (!value.length) return Comments.find({}, { limit: 5, sort: { created: -1 } }).fetch();
 	return Comments.find({ $text: { $search: value } }, { limit: 5, sort: { created: -1 } }).fetch();
-}
+};
 
 const commentsToggleDiscussionComments = (token, _id) => {
 	check(_id, String);
 	check(token, Match.Maybe(String));
 
 	const roles = ['editor', 'admin', 'commenter'];
-	if ((
-			!Meteor.userId()
-			&& !Roles.userIsInRole(Meteor.user(), roles)
-		)
-		&& !Meteor.users.findOne({
-			roles: roles,
-			'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(token),
-		})
+	if (!Meteor.users.findOne({
+		roles: { $elemMatch: { $in: roles } },
+		'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(token),
+	})
 	) {
-		throw new Meteor.Error('comment-insert', 'not-authorized');
+		throw new Meteor.Error('comment-toggle-discussion-comment', 'not-authorized');
 	}
 
 	const comment = Comments.findOne({_id});
@@ -201,15 +209,19 @@ const commentsToggleDiscussionComments = (token, _id) => {
 	if (typeof updater === 'undefined') updater = true;
 
 	if (comment) {
-		Comments.update({
-			_id
-		}, {
-			$set: {
-				discussionCommentsDisabled: updater,
-			},
-		});
+		try {
+			Comments.update({
+				_id
+			}, {
+				$set: {
+					discussionCommentsDisabled: updater,
+				},
+			});
+		} catch (err) {
+			throw new Meteor.Error('comment-toggle-discussion-comment', err);
+		}
 	}
-}
+};
 
 
 Meteor.methods({
--- server/methods/comments/comments.test.js
+++ server/methods/comments/comments.test.js
@@ -1,192 +1,491 @@
-// // meteor:
-// import { Meteor } from 'meteor/meteor';
-// import { Accounts } from 'meteor/accounts-base';
-// import { Roles } from 'meteor/alanning:roles';
+// meteor:
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { Roles } from 'meteor/alanning:roles';
+import { Random } from 'meteor/random';
 
-// // npm:
-// import { stub } from 'sinon';
-// import faker from 'faker';
+// npm:
+import { stub } from 'sinon';
+import faker from 'faker';
 
-// // api:
-// import Comments from '/imports/api/collections/comments';
-// import Commenters from '/imports/api/collections/commenters';
+// api:
+import Comments from '/imports/api/collections/comments';
+import Commenters from '/imports/api/collections/commenters';
 
-// // tested module:
-// import { commentsInsert, commentsUpdate, commentsRemove, commentsAddRevision, commentsRemoveRevision, commentsGetSuggestions, commentsToggleDiscussionComments } from './comments';
+// tested module:
+import * as commentMethods from './comments';
+
+function checkRole(roles, checkRoles) {
+	return roles.$elemMatch.$in.find(role => (checkRoles.indexOf(role) > -1));
+}
+
+describe('Comments methods API', () => {
+	describe('comments.insert', () => {
+		[
+			{
+				token: faker.random.uuid(),
+				comment: {}
+			}
+		].forEach((testCase, index) => {
+
+			const { token, comment } = testCase;
+
+			const commentId = faker.random.uuid();
+
+			const hashedLoginToken = faker.random.uuid();
+
+			const roles = ['editor', 'admin', 'commenter'];
+
+			describe(`Test Case ${index}`, () => {
+
+				beforeEach(() => {
+
+					stub(Meteor.users, 'findOne').callsFake((attr) => {
+						if (checkRole(attr.roles, roles)
+							&& attr['services.resume.loginTokens.hashedToken'] === hashedLoginToken) {
+							return true; 
+						}
+						return false;
+					});
+
+					Comments.insert = () => {};
+					stub(Comments, 'insert').callsFake(() => commentId);
+
+					stub(Accounts, '_hashLoginToken').callsFake(() => hashedLoginToken);
+
+				});
+
+				afterEach(() => {
+					Meteor.users.findOne.restore();
+					Comments.insert.restore();
+					Accounts._hashLoginToken.restore();
+				});
+
+				test('user with correct privileges not found, should return error', () => {
+
+					Accounts._hashLoginToken.restore();
+					stub(Accounts, '_hashLoginToken').callsFake(() => null);
+
+					expect(commentMethods.commentsInsert.bind(null, token, comment)).toThrow();
+				});
+
+				test('successful comment insert', () => {
+
+					expect(commentMethods.commentsInsert(token, comment)).toBe(commentId);
+				});
+			});
+		});
+	});
+
+	describe('comments.update', () => {
+		[
+			{
+				token: faker.random.uuid(),
+				commentId: faker.random.uuid(),
+				update: {}
+			}
+		].forEach((testCase, index) => {
+
+			const { token, commentId, update } = testCase;
+
+			const hashedLoginToken = faker.random.uuid();
+
+			const commenterId = faker.random.uuid();
+			const commenterSlug = faker.helpers.slugify(faker.name.firstName());
+
+			const comment = {
+				_id: commentId,
+				commenters: [{
+					_id: commenterId,
+					slug: commenterSlug
+				}],
+			};
+
+			const commenters = [{
+				_id: commenterId,
+				slug: commenterSlug,
+			}];
+
+			const user = {
+				canEditCommenters: [commenterId],
+			};
+
+			const roles = ['editor', 'admin', 'commenter'];
+
+			describe(`Test Case ${index}`, () => {
+
+				beforeEach(() => {
+
+					stub(Meteor.users, 'findOne').callsFake((attr) => {
+						if (checkRole(attr.roles, roles)
+							&& attr['services.resume.loginTokens.hashedToken'] === hashedLoginToken) {
+							return user;
+						}
+						return undefined;
+					});
+
+					Comments.update = () => {};
+					stub(Comments, 'update').callsFake(() => 1);
+
+					stub(Comments, 'findOne').callsFake(() => comment);
+
+					stub(Commenters, 'find').callsFake(() => ({
+						fetch: () => (commenters),
+					}));
+
+					stub(Accounts, '_hashLoginToken').callsFake(() => hashedLoginToken);
+
+				});
+
+				afterEach(() => {
+					Meteor.users.findOne.restore();
+					Comments.update.restore();
+					Comments.findOne.restore();
+					Commenters.find.restore();
+					Accounts._hashLoginToken.restore();
+				});
+
+				test('user with correct privileges not found, should return error', () => {
+
+					Accounts._hashLoginToken.restore();
+					stub(Accounts, '_hashLoginToken').callsFake(() => null);
+
+					expect(commentMethods.commentsUpdate.bind(null, token, commentId, update)).toThrow();
+				});
+
+				test('wrong commentId passed to method, should return error', () => {
+
+					Comments.findOne.restore();
+					stub(Comments, 'findOne').callsFake(() => null);
+
+					expect(commentMethods.commentsUpdate.bind(null, token, commentId, update)).toThrow();
+				});
+
+				test('user is not an owner of the comment, should return error', () => {
+
+					Meteor.users.findOne.restore();
+					const newUser = {
+						canEditCommenters: [faker.random.uuid(), faker.random.uuid()],
+					};
+					stub(Meteor.users, 'findOne').callsFake((attr) => {
+						if (checkRole(attr.roles, roles)
+							&& attr['services.resume.loginTokens.hashedToken'] === hashedLoginToken) {
+							return newUser;
+						}
+						return undefined;
+					});
 
 
-// describe('Comments methods API', () => {
-// 	describe('comments.insert', () => {
-// 		[
-// 			{
-// 				token: faker.random.uuid(),
-// 				comment: {}
-// 			}
-// 		].forEach((testCase, index) => {
+					expect(commentMethods.commentsUpdate.bind(null, token, commentId, update)).toThrow();
+				});
 
-// 			const { token, comment } = testCase;
+				test('successful comment update', () => {
 
-// 			const commentId = faker.random.uuid();
+					expect(commentMethods.commentsUpdate(token, commentId, update)).toBe(commentId
+						);
+				});
+			});
+		});
+	});
 
-// 			const hashedLoginToken = faker.random.uuid();
+	describe('comments.remove', () => {
+		[
+			{
+				token: faker.random.uuid(),
+				commentId: faker.random.uuid(),
+			}
+		].forEach((testCase, index) => {
 
-// 			const roles = ['editor', 'admin', 'commenter'];
+			const { token, commentId } = testCase;
 
-// 			describe(`Test Case ${index}`, () => {
+			const hashedLoginToken = faker.random.uuid();
 
-// 				beforeEach(() => {
+			const roles = ['editor', 'admin', 'commenter'];
 
-// 					stub(Meteor.users, 'findOne').callsFake((attr) => {
-// 						if (roles.indexOf(attr.roles) > -1
-// 							&& attr['services.resume.loginTokens.hashedToken'] === hashedLoginToken) {
-// 							return true; 
-// 						}
-// 						return false;
-// 					});
+			describe(`Test Case ${index}`, () => {
 
-// 					Comments.insert = () => {};
-// 					stub(Comments, 'insert').callsFake(() => commentId);
+				beforeEach(() => {
 
-// 					stub(Accounts, '_hashLoginToken').callsFake(() => hashedLoginToken);
+					stub(Meteor.users, 'findOne').callsFake((attr) => {
+						if (checkRole(attr.roles, roles)
+							&& attr['services.resume.loginTokens.hashedToken'] === hashedLoginToken) {
+							return true; 
+						}
+						return false;
+					});
 
-// 				});
+					Comments.remove = () => {};
+					stub(Comments, 'remove').callsFake(() => 1);
 
-// 				afterEach(() => {
-// 					Meteor.users.findOne.restore();
-// 					Comments.insert.restore();
-// 					Accounts._hashLoginToken.restore();
-// 				});
+					stub(Accounts, '_hashLoginToken').callsFake(() => hashedLoginToken);
 
-// 				test(`user with correct privileges not found, should return error`, () => {
+				});
 
-// 					Accounts._hashLoginToken.restore();
-// 					stub(Accounts, '_hashLoginToken').callsFake(() => null);
+				afterEach(() => {
+					Meteor.users.findOne.restore();
+					Comments.remove.restore();
+					Accounts._hashLoginToken.restore();
+				});
 
-// 					expect(commentsInsert.bind(null, token, comment)).toThrow();
-// 				});
+				test('user with correct privileges not found, should return error', () => {
 
-// 				test(`successful comment insert`, () => {
+					Accounts._hashLoginToken.restore();
+					stub(Accounts, '_hashLoginToken').callsFake(() => null);
 
-// 					expect(commentsInsert(token, comment)).toBe(commentId);
-// 				});
-// 			});
-// 		});
-// 	});
+					expect(commentMethods.commentsRemove.bind(null, token, commentId)).toThrow();
+				});
 
-// 	describe('comments.update', () => {
-// 		[
-// 			{
-// 				token: faker.random.uuid(),
-// 				_id: faker.random.uuid(),
-// 				book: {
-// 					title: faker.name.title(),
-// 					slug: faker.helpers.slugify(faker.name.title()),
-// 					author: faker.name.firstName(),
-// 					chapters: faker.random.number(),
-// 					coverImage: faker.random.uuid(),
-// 					tenantId: faker.random.uuid(),
-// 					year: faker.random.number(),
-// 					publisher: faker.random.uuid(),
-// 					citation: faker.lorem.text(),
-// 				}
-// 			}
-// 		].forEach((testCase, index) => {
+				test('successful comment remove', () => {
 
-// 			const { token, _id, book } = testCase;
+					expect(commentMethods.commentsRemove(token, commentId)).toBe(commentId);
+				});
+			});
+		});
+	});
 
-// 			const hashedLoginToken = faker.random.uuid();
+	describe('comments.add.revision', () => {
+		[
+			{
+				commentId: faker.random.uuid(),
+				revision: {}
+			}
+		].forEach((testCase, index) => {
 
-// 			describe(`Test Case ${index}`, () => {
+			const { commentId, revision } = testCase;
 
-// 				beforeEach(() => {
+			const commenterId = faker.random.uuid();
+			const commenterSlug = faker.helpers.slugify(faker.name.firstName());
 
-// 					stub(Meteor.users, 'findOne').callsFake((attr) => {
-// 						if (attr.roles === 'admin'
-// 							&& attr['services.resume.loginTokens.hashedToken'] === hashedLoginToken) {
-// 							return true; 
-// 						}
-// 						return false;
-// 					});
+			const comment = {
+				_id: commentId,
+				commenters: [{
+					_id: commenterId,
+					slug: commenterSlug
+				}],
+			};
 
-// 					Books.update = () => {};
-// 					stub(Books, 'update').callsFake(() => 1);
+			const commenters = [{
+				_id: commenterId,
+				slug: commenterSlug,
+			}];
 
-// 					stub(Accounts, '_hashLoginToken').callsFake(() => hashedLoginToken);
+			const user = {
+				_id: faker.random.uuid(),
+				canEditCommenters: [commenterId],
+			};
 
-// 				});
+			const revisionId = faker.random.uuid();
 
-// 				afterEach(() => {
-// 					Meteor.users.findOne.restore();
-// 					Books.update.restore();
-// 					Accounts._hashLoginToken.restore();
-// 				});
+			describe(`Test Case ${index}`, () => {
 
-// 				test(`user with correct privileges not found, should return error`, () => {
+				beforeEach(() => {
 
-// 					Accounts._hashLoginToken.restore();
-// 					stub(Accounts, '_hashLoginToken').callsFake(() => null);
+					stub(Meteor, 'user').callsFake(() => user);
 
-// 					expect(bookUpdate.bind(null, token, _id, book)).toThrow();
-// 				});
+					stub(Roles, 'userIsInRole').callsFake(() => true);
 
-// 				test(`successful book update`, () => {
+					Comments.update = () => {};
+					stub(Comments, 'update').callsFake(() => 1);
 
-// 					expect(bookUpdate(token, _id, book)).toBe(1);
-// 				});
-// 			});
-// 		});
-// 	});
+					stub(Comments, 'findOne').callsFake(() => comment);
 
-// 	describe('books.remove', () => {
-// 		[
-// 			{
-// 				token: faker.random.uuid(),
-// 				bookId: faker.random.uuid(),
-// 			}
-// 		].forEach((testCase, index) => {
+					stub(Commenters, 'find').callsFake(() => ({
+						fetch: () => (commenters),
+					}));
 
-// 			const { token, bookId } = testCase;
+					stub(Random, 'id').callsFake(() => revisionId);
 
-// 			const hashedLoginToken = faker.random.uuid();
+				});
 
-// 			describe(`Test Case ${index}`, () => {
+				afterEach(() => {
+					Meteor.user.restore();
+					Roles.userIsInRole.restore();
+					Comments.update.restore();
+					Comments.findOne.restore();
+					Commenters.find.restore();
+					Random.id.restore();
+				});
 
-// 				beforeEach(() => {
+				test('user not found, should return error', () => {
 
-// 					stub(Meteor.users, 'findOne').callsFake((attr) => {
-// 						if (attr.roles === 'admin'
-// 							&& attr['services.resume.loginTokens.hashedToken'] === hashedLoginToken) {
-// 							return true; 
-// 						}
-// 						return false;
-// 					});
+					Meteor.user.restore();
+					stub(Meteor, 'user').callsFake(() => null);
 
-// 					Books.remove = () => {};
-// 					stub(Books, 'remove').callsFake(() => 1);
+					expect(commentMethods.commentsAddRevision.bind(null, commentId, revision)).toThrow();
+				});
 
-// 					stub(Accounts, '_hashLoginToken').callsFake(() => hashedLoginToken);
+				test('user with incorrect roles, should return error', () => {
 
-// 				});
+					Roles.userIsInRole.restore();
+					stub(Roles, 'userIsInRole').callsFake(() => false);
 
-// 				afterEach(() => {
-// 					Meteor.users.findOne.restore();
-// 					Books.remove.restore();
-// 					Accounts._hashLoginToken.restore();
-// 				});
+					expect(commentMethods.commentsAddRevision.bind(null, commentId, revision)).toThrow();
+				});
 
-// 				test(`user with correct privileges not found, should return error`, () => {
+				test('wrong commentId passed to method, should return error', () => {
 
-// 					Accounts._hashLoginToken.restore();
-// 					stub(Accounts, '_hashLoginToken').callsFake(() => null);
+					Comments.findOne.restore();
+					stub(Comments, 'findOne').callsFake(() => null);
 
-// 					expect(bookRemove.bind(null, token, bookId)).toThrow();
-// 				});
+					expect(commentMethods.commentsAddRevision.bind(null, commentId, revision)).toThrow();
+				});
 
-// 				test(`successful book remove`, () => {
+				test('user is not an owner of the comment, should return error', () => {
 
-// 					expect(bookRemove(token, bookId)).toBe(1);
-// 				});
-// 			});
-// 		});
-// 	});
-// });
+					Meteor.user.restore();
+					const newUser = {
+						_id: faker.random.uuid(),
+						canEditCommenters: [faker.random.uuid(), faker.random.uuid()],
+					};
+					stub(Meteor, 'user').callsFake(() => newUser);
+
+
+					expect(commentMethods.commentsAddRevision.bind(null, commentId, revision)).toThrow();
+				});
+
+				test('successful comment insert', () => {
+
+					expect(commentMethods.commentsAddRevision(commentId, revision)).toBe(revisionId);
+				});
+			});
+		});
+	});
+
+	describe('comments.remove.revision', () => {
+		[
+			{
+				commentId: faker.random.uuid(),
+				revision: {}
+			}
+		].forEach((testCase, index) => {
+
+			const { commentId, revision } = testCase;
+
+			const commenterId = faker.random.uuid();
+			const commenterSlug = faker.helpers.slugify(faker.name.firstName());
+
+			const comment = {
+				_id: commentId,
+				commenters: [{
+					_id: commenterId,
+					slug: commenterSlug
+				}],
+			};
+
+			const commenters = [{
+				_id: commenterId,
+				slug: commenterSlug,
+			}];
+
+			const user = {
+				_id: faker.random.uuid(),
+				canEditCommenters: [commenterId],
+			};
+
+			const revisionId = faker.random.uuid();
+
+			describe(`Test Case ${index}`, () => {
+
+				beforeEach(() => {
+
+					stub(Meteor, 'user').callsFake(() => user);
+
+					stub(Roles, 'userIsInRole').callsFake(() => true);
+
+					Comments.update = () => {};
+					stub(Comments, 'update').callsFake(() => 1);
+
+				});
+
+				afterEach(() => {
+					Meteor.user.restore();
+					Roles.userIsInRole.restore();
+					Comments.update.restore();
+				});
+
+				test('user not found, should return error', () => {
+
+					Meteor.user.restore();
+					stub(Meteor, 'user').callsFake(() => null);
+
+					expect(commentMethods.commentsRemoveRevision.bind(null, commentId, revision)).toThrow();
+				});
+
+				test('user with incorrect roles, should return error', () => {
+
+					Roles.userIsInRole.restore();
+					stub(Roles, 'userIsInRole').callsFake(() => false);
+
+					expect(commentMethods.commentsRemoveRevision.bind(null, commentId, revision)).toThrow();
+				});
+
+				test('successful comment insert', () => {
+
+					expect(commentMethods.commentsRemoveRevision(commentId, revision)).toBe(undefined);
+				});
+			});
+		});
+	});
+
+	describe('comments.toggleDiscussionComments', () => {
+		[
+			{
+				token: faker.random.uuid(),
+				_id: faker.random.uuid(),
+			}
+		].forEach((testCase, index) => {
+
+			const { token, _id } = testCase;
+
+			const hashedLoginToken = faker.random.uuid();
+
+			const comment = {
+				discussionCommentsDisabled: true,
+			};
+
+			const roles = ['editor', 'admin', 'commenter'];
+
+			describe(`Test Case ${index}`, () => {
+
+				beforeEach(() => {
+
+					stub(Meteor.users, 'findOne').callsFake((attr) => {
+						if (checkRole(attr.roles, roles)
+							&& attr['services.resume.loginTokens.hashedToken'] === hashedLoginToken) {
+							return true; 
+						}
+						return false;
+					});
+
+					Comments.update = () => {};
+					stub(Comments, 'update').callsFake(() => 1);
+
+					stub(Comments, 'findOne').callsFake(() => comment);
+
+					stub(Accounts, '_hashLoginToken').callsFake(() => hashedLoginToken);
+
+				});
+
+				afterEach(() => {
+					Meteor.users.findOne.restore();
+					Comments.update.restore();
+					Comments.findOne.restore();
+					Accounts._hashLoginToken.restore();
+				});
+
+				test('user with correct privileges not found, should return error', () => {
+
+					Accounts._hashLoginToken.restore();
+					stub(Accounts, '_hashLoginToken').callsFake(() => null);
+
+					expect(commentMethods.commentsToggleDiscussionComments.bind(null, token, _id)).toThrow();
+				});
+
+				test('successful toggleDiscussionComments', () => {
+
+					expect(commentMethods.commentsToggleDiscussionComments(token, _id)).toBe(undefined);
+				});
+			});
+		});
+	});
+});

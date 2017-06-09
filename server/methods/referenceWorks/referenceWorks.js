4f38c321be6bd02b10396b4fcec3416dcf0f2005
Michał Pierzchlewicz <kontakt@michalpierzchlewicz.pl>
2017-06-09 17:31:59 +0200

    meteor methods tests: ReferenceWorks

---
+++ server/methods/referenceWorks/referenceWorks.js
@@ -0,0 +1,76 @@
+import { Meteor } from 'meteor/meteor';
+import { check, Match } from 'meteor/check';
+import { Accounts } from 'meteor/accounts-base';
+
+import ReferenceWorks from '/imports/api/collections/referenceWorks';
+
+const referenceWorksInsert = (token, referenceWork) => {
+	check(token, String);
+	check(referenceWork, {
+		title: String,
+		slug: String,
+		tenantId: Match.Maybe(String),
+		authors: Match.Maybe(Array),
+		coverImage: Match.Maybe(String),
+	});
+
+	if (
+			Meteor.users.findOne({
+				roles: 'admin',
+				'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(token),
+			})) {
+		return ReferenceWorks.insert(referenceWork);
+	}
+
+	throw new Meteor.Error('meteor-ddp-admin', 'Attempted publishing with invalid token');
+};
+
+const referenceWorksUpdate = (token, _id, referenceWork) => {
+	check(token, String);
+	check(_id, String);
+	check(referenceWork, {
+		title: String,
+		slug: String,
+		tenantId: Match.Maybe(String),
+		authors: Match.Maybe(Array),
+		coverImage: Match.Maybe(String),
+	});
+
+	if (
+			Meteor.users.findOne({
+				roles: 'admin',
+				'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(token),
+			})) {
+		return ReferenceWorks.update({
+			_id
+		}, {
+			$set: referenceWork,
+		});
+	}
+
+	throw new Meteor.Error('meteor-ddp-admin', 'Attempted publishing with invalid token');
+};
+
+const referenceWorksRemove = (token, referenceWorkId) => {
+	check(token, String);
+	check(referenceWorkId, String);
+
+	if (
+			Meteor.users.findOne({
+				roles: 'admin',
+				'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(token),
+			})) {
+		return ReferenceWorks.remove(referenceWorkId);
+	}
+
+	throw new Meteor.Error('meteor-ddp-admin', 'Attempted publishing with invalid token');
+};
+
+
+Meteor.methods({
+	'referenceWorks.insert': referenceWorksInsert,
+	'referenceWorks.update': referenceWorksUpdate,
+	'referenceWorks.remove': referenceWorksRemove,
+});
+
+export { referenceWorksInsert, referenceWorksUpdate, referenceWorksRemove };

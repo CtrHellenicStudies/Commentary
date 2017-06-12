1c172906c8e3d522f11d76dfaead1e19acfe3851
Luke Hollis <lukehollis@gmail.com>
2017-06-12 02:28:21 -0400

    add status for comments

--- server/publish/collections.js
+++ server/publish/collections.js
@@ -17,6 +17,8 @@ if (Meteor.isServer) {
 		check(skip, Number);
 		check(limit, Number);
 
+		query.status = 'publish';
+
 		return Comments.find(query, {
 			skip,
 			limit,
@@ -32,7 +34,9 @@ if (Meteor.isServer) {
 	Meteor.publish('comments.recent', (skip = 0, limit = 12) => {
 		check(skip, Number);
 		check(limit, Number);
-		return Comments.find({}, {
+		return Comments.find({
+			status: 'publish',
+		}, {
 			limit,
 			sort: {
 				updated: -1,
@@ -44,6 +48,7 @@ if (Meteor.isServer) {
 		check(limit, Number);
 		check(tenantId, Match.Maybe(String));
 		return Comments.find({
+			status: 'publish',
 			tenantId,
 		}, {
 			limit,
@@ -299,11 +304,32 @@ if (Meteor.isServer) {
 		});
 	});
 
+	Meteor.publish('annotations.publish', (skip = 0, limit = 100) => {
+		check(skip, Number);
+		check(limit, Number);
+
+		return Comments.find({
+			status: 'publish',
+			isAnnotation: true,
+		}, {
+			sort: {
+				tenantId: 1,
+				book: 1,
+				paragraphN: 1,
+				nLines: -1,
+			},
+			skip,
+			limit,
+		});
+	});
+
 	Meteor.publish('annotations.all', (skip = 0, limit = 100) => {
 		check(skip, Number);
 		check(limit, Number);
 
-		return Comments.find({ isAnnotation: true }, {
+		return Comments.find({
+			isAnnotation: true
+		}, {
 			sort: {
 				tenantId: 1,
 				book: 1,

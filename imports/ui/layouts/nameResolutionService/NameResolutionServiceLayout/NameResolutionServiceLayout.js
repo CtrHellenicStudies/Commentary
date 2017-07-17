37ae554ba29db853e0a8370b3db7e7e7e0e61ddd
Luke Hollis <lukehollis@gmail.com>
2017-07-17 07:01:36 -0400

    add name resolution service layout

---
+++ imports/ui/layouts/nameResolutionService/NameResolutionServiceLayout/NameResolutionServiceLayout.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createContainer } from 'meteor/react-meteor-data';
+
+
+
+const NameResolutionServiceLayout = props => {
+
+	resolve() {
+		window.location = this.props.resolveLink;
+	}
+
+	render() {
+		const { doi, urn } = this.props;
+
+		if (!doi || !urn) {
+			return (
+				<div>
+					Supply Digital Object Identifier (DOI) or Uniform Resource Name (URN) to resolve.
+				</div>
+			)
+		}
+
+		const identifier = doi || urn;
+
+		if (!resolveLink) {
+			return (
+				<div>
+					Could not resolve Persistent Identifier {identifier}. Please consult a systems administrator if you feel that this is in error.
+				</div>
+			)
+		}
+
+		return (
+			<div>
+				{this.resolve()}
+			</div>
+		)
+	}
+};
+
+const nameResolutionServiceLayoutContainer = createContainer(props => {
+	let urn;
+	let doi;
+	let resolveLink;
+
+	return {
+		resolveLink,
+	};
+}, NameResolutionServiceLayout);
+
+
+export default nameResolutionServiceLayoutContainer;

34478268674d718cdec092b6ccf5fb484218217e
Tyler Kirby <joseph.kirby12@ncf.edu>
2017-08-01 12:58:00 -0500

    added subscriptions to notification tab

--- imports/ui/components/user/ProfilePage/Notifications/Notifications.js
+++ imports/ui/components/user/ProfilePage/Notifications/Notifications.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
 
 class Notifications extends React.Component {
+	static propTypes = {
+		subscriptions: React.PropTypes.object
+	}
+
 	render () {
+		const { subscriptions } = this.props;
+		
 		return (
-			<div>
+			<div className="content primary">
 				<h2>Your Notifications</h2>
 				<hr className="user-divider" />
-				<div className="user-discussion-comments" />
+				<h2>Subscriptions</h2>
+				{subscriptions ?
+					<h3>You are subscibed to people!</h3>
+					:
+					<h3>You have no subscriptions</h3>
+				}
 			</div>
 		);
 	}

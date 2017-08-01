d5f6459d34d1fe905bb11b4fd07e1a53d54b3af8
Tyler Kirby <joseph.kirby12@ncf.edu>
2017-08-01 10:25:23 -0500

    refactored CommentUpperRight to stateful component and made subcribe button dynamic

--- imports/ui/components/commentary/comments/CommentUpper/CommentUpper.js
+++ imports/ui/components/commentary/comments/CommentUpper/CommentUpper.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AvatarIcon from '/imports/ui/components/avatar/AvatarIcon';  
 import FontIcon from 'material-ui/FontIcon';
 import FlatButton from 'material-ui/FlatButton';
+import { Meteor } from 'meteor/meteor';
 
 
 /*
@@ -23,71 +24,85 @@ CommentUpperLeft.propTypes = {
 /*
 	BEGIN CommentUpperRight
 */
-const CommentUpperRight = props => (
-	<div className="comment-upper-right">
-		{props.commenters.map(commenter => (
-			<div
-				key={commenter._id}
-				className="comment-author"
-			>
-				{props.userCanEditCommenters.indexOf(commenter._id) > -1 ?
-					<FlatButton
-						label="Edit comment"
-						href={`/commentary/${props.commentId}/edit`}
-						icon={<FontIcon className="mdi mdi-pen" />}
-					/>
-					:
-					''
-				}
-				<div className={'comment-author-text'}>
-					<a href={`/commenters/${commenter.slug}`}>
-						<span className="comment-author-name">{commenter.name}</span>
-					</a>
-					<span>
-						{props.updateDate}
-					</span>
-				</div>
-				<div className="comment-author-image-wrap paper-shadow">
-					<a href={`/commenters/${commenter.slug}`}>
-						<AvatarIcon
-							avatar={
-								(commenter && 'avatar' in commenter) ?
-								commenter.avatar.src
-								: null
-							}
-						/>
-					</a>
-				</div>
-				<div>
-					<FlatButton
-						label="Subscribe"
-					/>
-				</div>
+class CommentUpperRight extends React.Component {
+	componentWillMount() {
+		this.setState({
+			loggedIn: Meteor.user()
+		});
+	}
+
+	static propTypes = {
+		commenters: React.PropTypes.arrayOf(React.PropTypes.shape({
+			_id: React.PropTypes.string.isRequired,
+			slug: React.PropTypes.string.isRequired,
+			name: React.PropTypes.string.isRequired,
+			avatar: React.PropTypes.shape({
+				src: React.PropTypes.string.isRequired,
+			})
+		})).isRequired,
+		commentId: React.PropTypes.string.isRequired,
+		updateDate: React.PropTypes.string.isRequired,
+		userCanEditCommenters: React.PropTypes.arrayOf(React.PropTypes.string)
+	}
+
+	static defaultProps = {
+		userCanEditCommenters: []
+	}
+
+	render() {
+		const {commenters, userCanEditCommenters, commentId, updateDate} = this.props;
+		const {loggedIn} = this.state;
+
+		return (
+			<div className="comment-upper-right">
+				{commenters.map(commenter => (
+					<div
+						key={commenter._id}
+						className="comment-author"
+					>
+						{userCanEditCommenters.indexOf(commenter._id) > -1 ?
+							<FlatButton
+								label="Edit comment"
+								href={`/commentary/${commentId}/edit`}
+								icon={<FontIcon className="mdi mdi-pen" />}
+							/>
+							:
+							''
+						}
+						<div className={'comment-author-text'}>
+							<a href={`/commenters/${commenter.slug}`}>
+								<span className="comment-author-name">{commenter.name}</span>
+							</a>
+							<span>
+								{updateDate}
+							</span>
+						</div>
+						<div className="comment-author-image-wrap paper-shadow">
+							<a href={`/commenters/${commenter.slug}`}>
+								<AvatarIcon
+									avatar={
+										(commenter && 'avatar' in commenter) ?
+										commenter.avatar.src
+										: null
+									}
+								/>
+							</a>
+						</div>
+						{loggedIn ?
+							<div>
+								<FlatButton
+									label="Subscribe"
+								/>
+							</div>
+						:
+							''
+						}
+					</div>
+				))}
 			</div>
-		))}
-	</div>
-);
-CommentUpperRight.propTypes = {
-	commenters: React.PropTypes.arrayOf(React.PropTypes.shape({
-		_id: React.PropTypes.string.isRequired,
-		slug: React.PropTypes.string.isRequired,
-		name: React.PropTypes.string.isRequired,
-		avatar: React.PropTypes.shape({
-			src: React.PropTypes.string.isRequired,
-		})
-	})).isRequired,
-	commentId: React.PropTypes.string.isRequired,
-	updateDate: React.PropTypes.string.isRequired,
-	userCanEditCommenters: React.PropTypes.arrayOf(React.PropTypes.string),
-};
-CommentUpperRight.defaultProps = {
-	userCanEditCommenters: [],
-};
-/*
-	END CommentUpperRight
-*/
-
-
+		);
+	}
+}
 
 /*
 	BEGIN CommentUpper

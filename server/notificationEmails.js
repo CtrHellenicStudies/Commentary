803e8852bb5f8a566ae9dcf2d393d0597ab6f3cb
Tyler Kirby <joseph.kirby12@ncf.edu>
2017-08-21 16:30:48 -0500

    finished adding cron tasks

--- server/notificationEmails.js
+++ server/notificationEmails.js
@@ -1,21 +1,51 @@
 import { Meteor } from 'meteor/meteor';
+import { Email } from 'meteor/email';
 import { SyncedCron } from 'meteor/percolate:synced-cron';
+
+function sendBatchNotificationEmails(routine) {
+	const subscribedUsers = Meteor.users.find(
+		{$and: 
+		[
+			{batchNotification: routine}, 
+			{'subscriptions.notifications.0': {$exists: true}},
+			{emails: {$exists: true}}
+		]
+		}
+	).fetch();
+
+	subscribedUsers.forEach(user => {
+		const numberOfNotifications = user.subscriptions.notifications.length;
+		
+		let username = 'Commentary User';
+		if (user.profile.name) {
+			username = user.profile.name;
+		} else if (user.username) {
+			username = user.username;
+		}
+
+		const from = 'no-reply@ahcip.chs.harvard.edu';
+		const to = user.emails[0].address;
+		const subject = 'Notifications';
+		const text = `
+		Dear ${username},
+
+		You have ${numberOfNotifications} ${numberOfNotifications > 1 ? 'notifications' : 'notification'}.
+		Please review your notifications at ahcip.chs.harvard.edu.
+
+		You can change how often you receive these emails in your account settings.
+		`;
+
+		Email.send({from, to, subject, text});
+	}
+);
+
+}
 
 SyncedCron.add({
 	name: 'Send daily notification email',
 	schedule: parser => parser.text('at 9:00am'),
 	job: () => {
-		const subcribedUsers = Meteor.users.find(
-			{$and: 
-			[
-				{batchNotification: 'daily'}, 
-				{'subscriptions.notifications.0': {$exists: true}}
-			]
-			}
-		);
-		for (user of subscribedUsers) {
-
-		}
+		sendBatchNotificationEmails('daily');
 	}
 });
 
@@ -23,15 +53,7 @@ SyncedCron.add({
 	name: 'Send weekly notification email',
 	schedule: parser => parser.text('at 9:00am on Monday'),
 	job: () => {
-		const subcribedUsers = Meteor.users.find(
-			{$and: 
-			[
-				{batchNotification: 'weekly'}, 
-				{'subscriptions.notifications.0': {$exists: true}}
-			]
-			}
-		);
-		
+		sendBatchNotificationEmails('weekly');
 	}
 });
 
@@ -39,14 +61,6 @@ SyncedCron.add({
 	name: 'Send monthly notification email',
 	schedule: parser => parser.text('on the first day of the month at 9:00am'),
 	job: () => {
-		const subcribedUsers = Meteor.users.find(
-			{$and: 
-			[
-				{batchNotification: 'monthly'}, 
-				{'subscriptions.notifications.0': {$exists: true}}
-			]
-			}
-		);
-		
+		sendBatchNotificationEmails('monthly');
 	}
 });

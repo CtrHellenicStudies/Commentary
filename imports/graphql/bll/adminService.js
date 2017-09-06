50f0be6e2e5a1035de48c72ce1e63fded6617108
Igor <pasieczny.igor@gmail.com>
2017-09-06 11:12:56 +0200

    add no-token scenario to admin service

--- imports/graphql/bll/adminService.js
+++ imports/graphql/bll/adminService.js
@@ -1,9 +1,11 @@
+import { Accounts } from 'meteor/accounts-base';
+
 export default class AdminService {
 	constructor(props) {
-		this.token = props.token;
+		this.token = props.token ? props.token : '';
 		this.user = Meteor.users.findOne({
-			'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(props.token),
+			'services.resume.loginTokens.hashedToken': Accounts._hashLoginToken(this.token),
 		});
-		this.userIsAdmin = this.user.roles.indexOf('admin') !== -1;
+		this.userIsAdmin = this.user ? this.user.roles.indexOf('admin') !== -1 : false;
 	}
 }

afdb2429b6507d5118f37593838b04a453f51761
Igor <pasieczny.igor@gmail.com>
2017-09-21 20:59:32 +0200

    add translationNodes collection

---
+++ imports/models/translationNodes.js
@@ -0,0 +1,48 @@
+import { Meteor } from 'meteor/meteor';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+const TranslationNodes = new Meteor.Collection('translationNodes');
+
+TranslationNodes.schema = new SimpleSchema({
+	tenantId: {
+		type: String,
+		optional: true
+	},
+	created: {
+		type: Date,
+		optional: true
+	},
+	updated: {
+		type: Date,
+		optional: true
+	},
+	author: {
+		type: String,
+		optional: true
+	},
+	work: {
+		type: String,
+		optional: true
+	},
+	subwork: {
+		type: Number,
+		optional: true
+	},
+	n: {
+		type: Number,
+		optional: true
+	},
+	text: {
+		type: String,
+		optional: true
+	}
+});
+
+TranslationNodes.attachSchema(TranslationNodes.schema);
+
+TranslationNodes.attachBehaviour('timestampable', {
+	createdAt: 'created',
+	updatedAt: 'updated',
+});
+
+export default TranslationNodes;

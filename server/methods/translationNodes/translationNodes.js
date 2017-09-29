d9c97f178d4e8770375b860e8f4190f987e047d1
Igor <pasieczny.igor@gmail.com>
2017-09-29 15:43:25 +0200

    clean some code

--- server/methods/translationNodes/translationNodes.js
+++ server/methods/translationNodes/translationNodes.js
@@ -4,21 +4,6 @@ import TranslationNodes from '/imports/models/translationNodes';
 import Works from '/imports/models/works';
 import { getAuthorizedUser } from '../helpers';
 
-const translationsNodeInsert = (token, translationNode) => {
-	check(token, String);
-	check(translationNode, Object);
-
-	const roles = ['editor', 'admin', 'commenter'];
-	const user = getAuthorizedUser(roles, token);
-
-	// TODO: remove this after changing to workId instead of a slug
-	const newTranslationNode = translationNode;
-	const workSlug = Works.findOne(translationNode.work).slug;
-	newTranslationNode.work = workSlug;
-
-	return TranslationNodes.insert(newTranslationNode);
-};
-
 const translationsNodeUpdate = (token, translationNode) => {
 	check(token, String);
 	check(translationNode, Object);
@@ -90,7 +75,6 @@ const getTranslationNodesAuthors = (tenantId, workId, subwork) => {
 };
 
 Meteor.methods({
-	'translationNode.insert': translationsNodeInsert,
 	'translationNode.update': translationsNodeUpdate,
 	'translationNode.remove': translationsNodeRemove,
 	'translationNodes.getAuthors': getTranslationNodesAuthors,
@@ -99,7 +83,6 @@ Meteor.methods({
 });
 
 export {
-	translationsNodeInsert,
 	translationsNodeUpdate,
 	getTranslationNodesAuthors,
 	addTranslationAuthor,

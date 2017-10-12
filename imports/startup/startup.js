d31318ddeeba1fb92a1d8c3d7ceae92bf19ba5f4
Igor <pasieczny.igor@gmail.com>
2017-10-12 18:49:58 +0200

    remove router file import

--- imports/startup/startup.js
+++ imports/startup/startup.js
@@ -12,7 +12,6 @@ import '/imports/lib/_config/accounts';
 import '/imports/lib/_config/emails';
 import '/imports/lib/_config/i18n';
 import '/imports/lib/_config/oauth';
-import '/imports/lib/router';
 
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';

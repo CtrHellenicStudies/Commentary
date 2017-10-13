3e298c3ab0200dc5de6b3dc30f1aaf0a8b97f0f0
Igor <pasieczny.igor@gmail.com>
2017-10-13 12:39:48 +0200

    add enter key handling, clear name input after submit

--- imports/ui/components/editor/textNodes/MultilineDialog/MultilineDialog.js
+++ imports/ui/components/editor/textNodes/MultilineDialog/MultilineDialog.js
@@ -32,13 +32,18 @@ class MultilineDialog extends React.Component {
 		});
 	}
 
-	handleSubmit() {
+	handleSubmit(event) {
+		event.preventDefault();
 		Meteor.call('multiline.insert', Cookies.get('loginToken'), this.state.edition, this.state.multiline, (err) => {
 			if (err) {
 				this.setState({
 					error: err
 				});
 				throw new Error(err);
+			} else {
+				this.setState({
+					multiline: ''
+				});
 			}
 		});
 	}
@@ -50,6 +55,10 @@ class MultilineDialog extends React.Component {
 					error: err
 				});
 				throw new Error(err);
+			} else {
+				this.setState({
+					multiline: ''
+				});
 			}
 		});
 	}
@@ -118,13 +127,15 @@ class MultilineDialog extends React.Component {
 						<label>
 							Add new
 						</label>
+						<form onSubmit={this.handleSubmit}>
 						<TextField
 							name="multiline"
-							defaultValue={this.state.multiline}
+							value={this.state.multiline}
 							onChange={this.setValue}
 							fullWidth
 							errorText={error}
 						/>
+						</form>
 					</div>
 
 				</div>

829107f36cf5f862eba8446f06eac364534a78e7
Luke Hollis <lukehollis@gmail.com>
2017-10-15 12:59:28 -0400

    add basic docstrings and initial redux integration

--- imports/reducers/index.js
+++ imports/reducers/index.js
@@ -1,3 +1,8 @@
+/**
+ * Initial redux integration, to be extended in subsequent stages of the project
+ */
+
+
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';

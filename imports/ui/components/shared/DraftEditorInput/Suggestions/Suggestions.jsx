340d82d8663075584ff1057055c67908f731b28b
posejdon <abujak@itsg.com.pl>
2017-10-20 10:16:11 -0500

    add video and pictures packages

--- imports/ui/components/shared/DraftEditorInput/Suggestions/Suggestions.jsx
+++ imports/ui/components/shared/DraftEditorInput/Suggestions/Suggestions.jsx
@@ -6,42 +6,8 @@ import createMentionPlugin, { defaultSuggestionsFilter } from 'draft-js-mention-
 import { createContainer } from 'meteor/react-meteor-data';
 import Keywords from '/imports/models/keywords';
 import { fromJS } from 'immutable';
+import Utils from '/imports/lib/utils';
 
-function _getSuggestionsFromComments(comments) {
-	const suggestions = [];
-
-	// if there are comments:
-	if (comments.length) {
-
-		// loop through all comments
-		// add suggestion for each comment
-		comments.forEach((comment) => {
-
-			// get the most recent revision
-			const revision = comment.revisions[comment.revisions.length - 1];
-
-			const suggestion = {
-				// create suggestio name:
-				name: `"${revision.title}" -`,
-
-				// set link for suggestion
-				link: `/commentary?_id=${comment._id}`,
-
-				// set id for suggestion
-				id: comment._id,
-			};
-
-			// loop through commenters and add them to suggestion name
-			comment.commenters.forEach((commenter, i) => {
-				if (i === 0) suggestion.name += ` ${commenter.name}`;
-				else suggestion.name += `, ${commenter.name}`;
-			});
-
-			suggestions.push(suggestion);
-		});
-	}
-	return suggestions;
-}
 class Suggestions extends Component {
 
 	constructor(props){
@@ -60,7 +26,7 @@ class Suggestions extends Component {
 			if (err) throw new Meteor.Error(err);
 
 			// handle response:
-			const _mentions = _getSuggestionsFromComments(res);
+			const _mentions = Utils.getSuggestionsFromComments(res);
 
 			this.setState({
 				mentions: defaultSuggestionsFilter(value,fromJS(_mentions))

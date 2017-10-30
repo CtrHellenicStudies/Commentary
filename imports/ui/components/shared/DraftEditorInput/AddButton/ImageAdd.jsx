be0ad564de768f1bd4b5d1ab2df493ac4795789e
posejdon <abujak@itsg.com.pl>
2017-10-30 17:03:39 +0100

    adding images from file

--- imports/ui/components/shared/DraftEditorInput/AddButton/ImageAdd.jsx
+++ imports/ui/components/shared/DraftEditorInput/AddButton/ImageAdd.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import UploadImages from './UploadImage';
 
 export default class VideoAdd extends Component {
   // Start the popover closed
@@ -21,8 +22,11 @@ export default class VideoAdd extends Component {
   // Note: make sure whenever a click happens within the popover it is not closed
   onPopoverClick = () => {
     this.preventNextClose = true;
-  };
-
+  }
+  uploadedUrl(url){
+    const { editorState, onChange } = this.props;
+    onChange(this.props.modifier(editorState, url));
+  }
   openPopover = () => {
     if (!this.state.open) {
       this.preventNextClose = true;
@@ -59,13 +63,17 @@ export default class VideoAdd extends Component {
       <div>
         <RaisedButton className="draft-add-video-button" onMouseUp={this.openPopover}>{this.props.label}</RaisedButton>
         <div className={popoverClassName} onClick={this.onPopoverClick}>
-          <input
-            type="text"
-            placeholder="Paste the image url …"
-            onChange={this.changeUrl}
-            value={this.state.url}
-          />
-          <RaisedButton className="draft-add-video-confirm-button" onClick={this.addVideo}>Add</RaisedButton>
+          <div className="draft-image-from-url">
+            <input
+              type="text"
+              placeholder="Paste the image url …"
+              onChange={this.changeUrl}
+              value={this.state.url}
+            />
+            <RaisedButton className="draft-add-video-confirm-button" onClick={this.addVideo}>Add</RaisedButton>
+          </div>
+          <span>or</span>
+          <UploadImages uploadedUrl={this.uploadedUrl.bind(this)} />
         </div>
       </div>
     );

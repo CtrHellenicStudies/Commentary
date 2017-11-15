45773c21e182c6f60afb9304340f0a5e95eabe17
posejdon <abujak@itsg.com.pl>
2017-11-15 14:23:10 +0100

    fix route

--- imports/startup/client/App.js
+++ imports/startup/client/App.js
@@ -168,8 +168,7 @@ const routes = (props) => {
 				path="/:slug" render={(params) => {
 					const reservedRoutes = ['admin', 'sign-in', 'sign-up'];
 					if (reservedRoutes.indexOf(params.slug) === -1) {
-						//console.log(params);
-						//return <Page slug={params.match.params.slug} />;
+						return <Page slug={params.match.params.slug} />;
 					}
 					return <Redirect to="/" />;
 				}}

ad61299fa26c3e3acdfae2d7f7f8427019226af4
posejdon <abujak@itsg.com.pl>
2017-11-20 12:13:33 +0100

    remove discussions folder from meteor methods

--- imports/graphql/logic/DiscussionComments/helper.js
+++ imports/graphql/logic/DiscussionComments/helper.js
@@ -1,5 +1,12 @@
 import Utils from '/imports/lib/utils';
 import Config from '/imports/lib/_config/_config.js';
+import Meteor from 'meteor/meteor';
+import DiscussionComments from '/imports/models/discussionComments';
+import { sendBatchNotificationEmailsForComment } from '/server/notificationEmails';
+
+
+import { sendDiscussionCommentRejectEmail, sendDiscussionCommentPublishEmail } from './emails';
+
 
 
 /**
@@ -45,4 +52,63 @@ function sendReportMessage(comment, discussionComment) {
 		`,
 	});
 }
+function notifyAfterPublishRejection(id, status) {
+
+	const discussionComment = DiscussionComments.findOne(id);
+	const discussionComments = {};
+	const user = Meteor.users.findOne({ _id: discussionComment.userId });
+	const avatar = user.profile && user.profile.url ? user.profile.url : '/images/default_user.jpg';
+	const discussionCommentsArray = [];
+
+	const notification = {
+		message: status === 'publish' ? 'Your comment has been approved.' : 'Your comment has been rejected',
+		avatar: {src: avatar},
+		created: new Date(),
+		_id: new ObjectID().toString(),
+		slug: discussionComment.commentId
+	};
+
+	if (status === 'publish') {
+		sendDiscussionCommentPublishEmail(id);
+		DiscussionComments.find().map((disscuss) => {
+			if (disscuss.commentId === discussionComment.commentId && disscuss.userId !== user._id) {
+				discussionComments[disscuss.userId] = true;
+			}
+		});
+		for (const [key, value] of Object.entries(discussionComments)) {
+			discussionCommentsArray.push(key);
+		}
+		Meteor.users.update({ 
+			_id: {$in: discussionCommentsArray}
+		}, {
+			$push: {'subscriptions.notifications': notification
+			}
+		}, function sendNotificationEmail() {
+			sendBatchNotificationEmailsForComment(discussionComment.commentId, user._id);
+		});
+	} else if (status === 'trash') {
+		console.log('removed!');
+		sendDiscussionCommentRejectEmail(discussionCommentId);
+	}
+
+	const updateUser = Meteor.users.update({_id: discussionComment.userId}, {$push: {'subscriptions.notifications': notification}});
+
+}
+function sendNotification(configurationObj) {
+	console.log(configurationObj.to);
+	Email.send({
+		to: 'aniutka.pop@gmail.com',
+		from: Config.emails.from,
+		subject: 'Answer to your comment',
+		html: configurationObj.text
+	});
+
+}
+DiscussionComments.find().observeChanges({
+	changed: function changed(id, fields) {
+		if (!fields.status)			{ return; }
+		notifyAfterPublishRejection(id, fields.status);
+	}
+});
+	
 export { sendReportMessage };

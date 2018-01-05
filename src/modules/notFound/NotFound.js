3d356c5218ee62cdb4d0fef3d40b10b8fc911e22
posejdon <abujak@itsg.com.pl>
2018-01-05 17:43:14 +0100

    add more less files

--- src/modules/notFound/NotFound.js
+++ src/modules/notFound/NotFound.js
@@ -9,7 +9,6 @@ import Header from '../../components/header/Header';
 
 // components
 import BackgroundImageHolder from '../shared/BackgroundImageHolder';
-import '../notFound/notFound.import.css'
 
 class NotFound extends Component {
 

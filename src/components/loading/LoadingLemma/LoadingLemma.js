2d37925da1865e57f56a2a7aeb04327c5e08b6a9
Luke Hollis <lukehollis@gmail.com>
2018-02-26 07:40:57 -0500

    fix layout for loading lemma

--- src/components/loading/LoadingLemma/LoadingLemma.js
+++ src/components/loading/LoadingLemma/LoadingLemma.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 
 const LoadingLemma = () => (
-	<div className="lemma-loading">
-		<div className="lemma-loading-top" />
-		<div className="lemma-loading-bottom" />
+	<div className="comment-outer comment-lemma-comment-outer">
+		<article className="comment lemma-comment paper-shadow">
+			<div className="lemma-loading">
+				<div className="lemma-loading-top" />
+				<div className="lemma-loading-bottom" />
+			</div>
+		</article>
+		<div className="discussion-wrap" />
 	</div>
-	);
+);
 
 export default LoadingLemma;

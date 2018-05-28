c75d8792586155e8108adafb91abab218ef59c07
Luke Hollis <lukehollis@gmail.com>
2018-05-28 09:06:45 -0400

    add matchmedia mock to testing setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,11 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
 configure({ adapter: new Adapter() });
+
+window.matchMedia = window.matchMedia || function() {
+	return {
+		matches : false,
+		addListener : function() {},
+		removeListener: function() {},
+	};
+};
